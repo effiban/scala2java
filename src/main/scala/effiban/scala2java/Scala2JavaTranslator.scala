5718739243115368b8ef04bf09d07142b7173bd5
Effi Ban <effi.ban@kenshoo.com>
2022-07-09 19:56:42 +0300

    Support file output in Scala2JavaTranslator

--- src/main/scala/effiban/scala2java/Scala2JavaTranslator.scala
+++ src/main/scala/effiban/scala2java/Scala2JavaTranslator.scala
@@ -1,23 +1,36 @@
 package effiban.scala2java
 
 import effiban.scala2java.traversers.ScalaTreeTraversers
-import effiban.scala2java.writers.{ConsoleJavaWriter, JavaWriter}
+import effiban.scala2java.writers.{ConsoleJavaWriter, JavaWriter, JavaWriterImpl}
 
-import java.io.File
-import java.nio.file.Files
+import java.io.{File, FileWriter}
+import java.nio.file.{Files, Paths}
 import scala.meta.Source
 import scala.meta.inputs.Input
 
 object Scala2JavaTranslator {
 
-  def translate(scalaSourceFile: File): Unit = {
-    val text = Files.readString(scalaSourceFile.toPath)
-    val input = Input.VirtualFile(scalaSourceFile.getAbsolutePath, text)
+  def translate(scalaFile: File, maybeOutputDir: Option[File] = None): Unit = {
+    val text = Files.readString(scalaFile.toPath)
+    val input = Input.VirtualFile(scalaFile.getAbsolutePath, text)
     val sourceTree = input.parse[Source].get
 
-    implicit val javaWriter: JavaWriter = ConsoleJavaWriter
-    val traversers = new ScalaTreeTraversers
-    traversers.sourceTraverser.traverse(sourceTree)
+    implicit val javaWriter: JavaWriter = maybeOutputDir match {
+      case Some(outputDir) => createFileJavaWriter(scalaFile, outputDir)
+      case None => ConsoleJavaWriter
+    }
+    try {
+      new ScalaTreeTraversers().sourceTraverser.traverse(sourceTree)
+    } finally {
+      javaWriter.close()
+    }
+  }
+
+  private def createFileJavaWriter(scalaFile: File, outputDir: File) = {
+    outputDir.mkdirs()
+    val javaFileName = scalaFile.getName.replace("scala", "java")
+    val javaFile = Paths.get(outputDir.getAbsolutePath, javaFileName).toFile
+    new JavaWriterImpl(new FileWriter(javaFile))
   }
 }
 
