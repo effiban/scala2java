c971fadfa208bd3c00d8ac633dafb0cf06c74e0c
Effi Ban <effi.ban@kenshoo.com>
2022-07-09 19:16:04 +0300

    Extract Scala2JavaTranslator

--- src/main/scala/effiban/scala2java/Scala2JavaRunner.scala
+++ src/main/scala/effiban/scala2java/Scala2JavaRunner.scala
@@ -1,29 +1,13 @@
 package effiban.scala2java
 
-import effiban.scala2java.traversers.ScalaTreeTraversers
-import effiban.scala2java.writers.{ConsoleJavaWriter, JavaWriter}
-
-import scala.meta.Source
-import scala.meta.inputs.Input
+import java.nio.file.Paths
 
 object Scala2JavaRunner {
 
   def main(args: Array[String]): Unit = {
-
     val sourceFilePaths = args
-
-    val sourceTrees = sourceFilePaths.map(pathStr => {
-      val path = java.nio.file.Paths.get(pathStr)
-      val bytes = java.nio.file.Files.readAllBytes(path)
-      val text = new String(bytes, "UTF-8")
-      val input = Input.VirtualFile(path.toString, text)
-      input.parse[Source].get
-    })
-
-    implicit val javaWriter: JavaWriter = ConsoleJavaWriter
-    val traversers = new ScalaTreeTraversers
-
-    sourceTrees.foreach(traversers.sourceTraverser.traverse)
+    val sourceFiles = sourceFilePaths.map(pathStr => Paths.get(pathStr).toFile).toList
+    Scala2JavaTranslator.translate(sourceFiles)
   }
 }
 
