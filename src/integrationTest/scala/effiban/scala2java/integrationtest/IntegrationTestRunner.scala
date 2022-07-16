b6f4760393fc4f55482ffbd41fcb65bad5a51f0d
Effi Ban <effi.ban@kenshoo.com>
2022-07-17 00:45:29 +0300

    Improve integration test error message

--- src/integrationTest/scala/effiban/scala2java/integrationtest/IntegrationTestRunner.scala
+++ src/integrationTest/scala/effiban/scala2java/integrationtest/IntegrationTestRunner.scala
@@ -1,12 +1,12 @@
 package effiban.scala2java.integrationtest
 
 import effiban.scala2java.Scala2JavaTranslator.translate
+import effiban.scala2java.integrationtest.matchers.FileMatchers.equalContentsOf
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.{BeforeAndAfterAll, OptionValues}
 
 import java.nio.file.{Files, Path, Paths}
-import scala.jdk.CollectionConverters._
 import scala.jdk.StreamConverters._
 import scala.util.Using
 
@@ -54,24 +54,9 @@ class IntegrationTestRunner extends AnyFunSuite
       withClue(s"Output java file not found at $outputJavaPath") {
         outputJavaPath.toFile.exists() shouldBe true
       }
-      verifyJavaFileContents(outputJavaPath, expectedJavaPath)
+      outputJavaPath should equalContentsOf(expectedJavaPath)
     }
   }
 
   private def pathOf(basePath: Path, relativePath: Path, fileName: String) = Paths.get(basePath.toString, relativePath.toString, fileName)
-
-  private def verifyJavaFileContents(outputJavaPath: Path, expectedJavaPath: Path): Unit = {
-    val actualLines = Files.readAllLines(outputJavaPath).asScala
-    val expectedLines = Files.readAllLines(expectedJavaPath).asScala
-
-    withClue("Generated Java file has incorrect number of lines: ") {
-      actualLines.size shouldBe expectedLines.size
-    }
-
-    actualLines.indices.foreach(lineNum => {
-      withClue(s"Generated Java code doesn't match expected at line ${lineNum + 1}: ") {
-        actualLines(lineNum) shouldBe expectedLines(lineNum)
-      }
-    })
-  }
 }
