5b69dcd385609ebbb191129d1ad82db0f7e8410d
Effi Ban <effi.ban@kenshoo.com>
2022-08-26 19:49:32 +0300

    Fix LitTypeInferrer

--- src/test/scala/effiban/scala2java/typeinference/LitTypeInferrerTest.scala
+++ src/test/scala/effiban/scala2java/typeinference/LitTypeInferrerTest.scala
@@ -1,33 +1,40 @@
 package effiban.scala2java.typeinference
 
 import effiban.scala2java.testsuites.UnitTestSuite
+import effiban.scala2java.testtrees.TypeNames
 
 import scala.meta.{Lit, Type}
 
 class LitTypeInferrerTest extends UnitTestSuite {
 
   private val LiteralTypeMappings = Table(
-    ("Literal", "MaybeExpectedTypeValue"),
-    (Lit.Boolean(true), Some("Boolean")),
-    (Lit.Byte(1), Some("Byte")),
-    (Lit.Short(1), Some("Short")),
-    (Lit.Int(1), Some("Int")),
-    (Lit.Long(1), Some("Long")),
-    (Lit.Float(1.1f), Some("Float")),
-    (Lit.Double(1.1), Some("Double")),
-    (Lit.Char('a'), Some("Char")),
-    (Lit.String("abc"), Some("String")),
-    (Lit.Unit(), Some("Unit")),
-    (Lit.Null(), None),
-    (Lit.Symbol(scala.Symbol("sym")), None)
+    ("Literal", "ExpectedType"),
+    (Lit.Boolean(true), Type.Name("Boolean")),
+    (Lit.Byte(1), Type.Name("Byte")),
+    (Lit.Short(1), Type.Name("Short")),
+    (Lit.Int(1), Type.Name("Int")),
+    (Lit.Long(1), Type.Name("Long")),
+    (Lit.Float(1.1f), Type.Name("Float")),
+    (Lit.Double(1.1), Type.Name("Double")),
+    (Lit.Char('a'), Type.Name("Char")),
+    (Lit.String("abc"), TypeNames.String),
+    (Lit.Symbol(scala.Symbol("sym")), TypeNames.String),
+    (Lit.Unit(), Type.Name("Unit")),
+    (Lit.Null(), Type.AnonymousName())
   )
 
   forAll(LiteralTypeMappings) {
-    (literal: Lit, maybeExpectedTypeValue: Option[String]) => {
-      val maybeExpectedTypeName = maybeExpectedTypeValue.map(Type.Name(_))
-      test(s"Infer $literal should return $maybeExpectedTypeName") {
-        LitTypeInferrer.infer(literal).structure shouldBe maybeExpectedTypeName.structure
+    (literal: Lit, expectedType: Type) => {
+      test(s"Infer $literal should return ${typeToString(expectedType)}") {
+        LitTypeInferrer.infer(literal).value.structure shouldBe expectedType.structure
       }
+    }
+  }
+
+  private def typeToString(tpe: Type) = {
+    tpe match {
+      case Type.AnonymousName() => "<<anonymous>>"
+      case t => t.toString()
     }
   }
 }
