c262196abd73cc03d576bced6a96e54d0e241f08
Effi Ban <effi.ban@kenshoo.com>
2022-08-26 17:44:09 +0300

    Fix Java translation of method calls with multiple arg lists

--- src/main/scala/effiban/scala2java/transformers/ScalaToJavaTermApplyTransformer.scala
+++ src/main/scala/effiban/scala2java/transformers/ScalaToJavaTermApplyTransformer.scala
@@ -1,5 +1,6 @@
 package effiban.scala2java.transformers
 
+import scala.annotation.tailrec
 import scala.meta.Term
 
 trait ScalaToJavaTermApplyTransformer {
@@ -8,10 +9,13 @@ trait ScalaToJavaTermApplyTransformer {
 
 object ScalaToJavaTermApplyTransformer extends ScalaToJavaTermApplyTransformer {
 
-  // Transform any Scala-specific method invocations into an equivalent in Java
+  // Transform any method invocations which have a Scala-specific naming or style into Java equivalents
+  @tailrec
   override def transform(termApply: Term.Apply): Term.Apply = {
     termApply match {
       case Term.Apply(Term.Name("Range"), args) => Term.Apply(Term.Select(Term.Name("IntStream"), Term.Name("range")), args)
+      // Invocation of method with more than one param list
+      case Term.Apply(Term.Apply(fun, args1), args2) => transform(Term.Apply(fun, args1 ++ args2))
       case other => other
     }
   }
--- src/test/scala/effiban/scala2java/transformers/ScalaToJavaTermApplyTransformerTest.scala
+++ src/test/scala/effiban/scala2java/transformers/ScalaToJavaTermApplyTransformerTest.scala
@@ -7,6 +7,15 @@ import scala.meta.{Lit, Term}
 
 class ScalaToJavaTermApplyTransformerTest extends UnitTestSuite {
 
+  private val fun = Term.Name("foo")
+  private val arg1 = Term.Name("arg1")
+  private val arg2 = Term.Name("arg2")
+  private val arg3 = Term.Name("arg3")
+  private val arg4 = Term.Name("arg4")
+  private val arg5 = Term.Name("arg5")
+  private val arg6 = Term.Name("arg6")
+
+
   test("transform() of Range(...) should return IntStream.range(...)") {
     val scalaTermApply = Term.Apply(ScalaRangeTermName, List(Lit.Int(0), Lit.Int(10)))
     val expectedRangeTermApply = Term.Apply(Term.Select(JavaIntStreamTermName, JavaRangeTermName), List(Lit.Int(0), Lit.Int(10)))
@@ -18,5 +27,31 @@ class ScalaToJavaTermApplyTransformerTest extends UnitTestSuite {
     val termApply = Term.Apply(Term.Name("dummy"), List(Lit.Int(1)))
 
     ScalaToJavaTermApplyTransformer.transform(termApply).structure shouldBe termApply.structure
+  }
+
+  // foo(arg1, arg2)(arg3, arg4) ---> foo(arg1, arg2, arg3, arg4)
+  test("transform() of a 2-level nested Apply should return a single Apply with concatenated args") {
+    val scalaStyleTermApply =
+      Term.Apply(
+        Term.Apply(fun, List(arg1, arg2)),
+        List(arg3, arg4)
+    )
+    val expectedJavaStyleTermApply = Term.Apply(fun, List(arg1, arg2, arg3, arg4))
+
+    ScalaToJavaTermApplyTransformer.transform(scalaStyleTermApply).structure shouldBe expectedJavaStyleTermApply.structure
+  }
+
+  // foo(arg1, arg2)(arg3, arg4)(arg5, arg6) ---> foo(arg1, arg2, arg3, arg4, arg5, arg6)
+  test("transform() of a 3-level nested Apply should return a single Apply with concatenated args") {
+    val scalaStyleTermApply =
+      Term.Apply(
+        Term.Apply(
+          Term.Apply(fun, List(arg1, arg2)),
+          List(arg3, arg4)),
+        List(arg5, arg6)
+      )
+    val expectedJavaStyleTermApply = Term.Apply(fun, List(arg1, arg2, arg3, arg4, arg5, arg6))
+
+    ScalaToJavaTermApplyTransformer.transform(scalaStyleTermApply).structure shouldBe expectedJavaStyleTermApply.structure
   }
 }
