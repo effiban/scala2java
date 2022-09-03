88cb51ac57988f371f4b36442202de134f87881f
Effi Ban <effi.ban@kenshoo.com>
2022-09-03 05:43:26 +0300

    Extend JavaTermClassifier to JavaStatClassifier

--- src/test/scala/effiban/scala2java/classifiers/JavaStatClassifierTest.scala
+++ src/test/scala/effiban/scala2java/classifiers/JavaStatClassifierTest.scala
@@ -1,15 +1,15 @@
 package effiban.scala2java.classifiers
 
-import effiban.scala2java.classifiers.JavaTermClassifier.requiresStatementEnd
+import effiban.scala2java.classifiers.JavaStatClassifier.requiresEndDelimiter
 import effiban.scala2java.testsuites.UnitTestSuite
-import effiban.scala2java.testtrees.Selfs
+import effiban.scala2java.testtrees.{Selfs, TypeNames}
 
 import scala.meta.Enumerator.Generator
 import scala.meta.Mod.Annot
 import scala.meta.Term.{Apply, ApplyType, ApplyUnary, Assign, Block, Eta, For, ForYield, If, NewAnonymous, Super, This}
-import scala.meta.{Case, Init, Lit, Name, Pat, Template, Term, Type}
+import scala.meta.{Case, Decl, Defn, Import, Importee, Importer, Init, Lit, Name, Pat, Stat, Template, Term, Type}
 
-class JavaTermClassifierTest extends UnitTestSuite {
+class JavaStatClassifierTest extends UnitTestSuite {
 
   private val TheThis = This(Name.Indeterminate("MyName"))
   private val TheSuper = Super(thisp = Name.Indeterminate("superName"), superp = Name.Anonymous())
@@ -47,9 +47,16 @@ class JavaTermClassifierTest extends UnitTestSuite {
   private val AnonymousFunctionWithBlockOfOne = Term.AnonymousFunction(Block(List(Apply(Term.Name("foo"), Nil))))
   private val AnonymousFunctionWithBlockOfTwo = Term.AnonymousFunction(Block(List(Apply(Term.Name("foo1"), Nil), Apply(Term.Name("foo2"), Nil))))
   private val ThePartialFunction = Term.PartialFunction(List(Case(Lit.Int(1), None, Lit.String("one"))))
+  private val TheImport = Import(List(Importer(Term.Name("A"), List(Importee.Name(Name.Indeterminate("B"))))))
+  private val TheDeclVal = Decl.Val(Nil, List(Pat.Var(Term.Name("x"))), TypeNames.Int)
+  private val TheDefnVal = Defn.Val(Nil, List(Pat.Var(Term.Name("x"))), None, Lit.Int(3))
+  private val TheDeclVar = Decl.Var(Nil, List(Pat.Var(Term.Name("x"))), TypeNames.Int)
+  private val TheDefnVar = Defn.Var(Nil, List(Pat.Var(Term.Name("x"))), None, Some(Lit.Int(3)))
+  private val TheDeclDef = Decl.Def(Nil, Term.Name("foo"), Nil, List(Nil), TypeNames.Int)
+  private val TheDefnDef = Defn.Def(Nil, Term.Name("foo"), Nil, List(Nil), Some(TypeNames.Int), Term.Apply(Term.Name("bar"), Nil))
 
 
-  private val RequiresStatementEndScenarios = Table(
+  private val RequiresEndDelimiterScenarios = Table(
     ("Description", "Term", "ExpectedResult"),
     ("This", TheThis, true),
     ("Super", TheSuper, true),
@@ -63,7 +70,7 @@ class JavaTermClassifierTest extends UnitTestSuite {
     ("Return", TheReturn, true),
     ("Ascribe", TheAscribe, true),
     ("Tuple", TheTuple, true),
-    ("For", TheFor, false),
+    ("For", TheFor, true),
     ("ForYield", TheForYield, true),
     ("Do", TheDo, true),
     ("New", TheNew, true),
@@ -86,13 +93,20 @@ class JavaTermClassifierTest extends UnitTestSuite {
     ("AnonymousFunctionWithStatement", AnonymousFunctionWithStatement, true),
     ("AnonymousFunctionWithBlockOfOne", AnonymousFunctionWithBlockOfOne, true),
     ("AnonymousFunctionWithBlockOfTwo", AnonymousFunctionWithBlockOfTwo, false),
-    ("PartialFunction", ThePartialFunction, false)
+    ("PartialFunction", ThePartialFunction, false),
+    ("Import", TheImport, true),
+    ("DeclVal", TheDeclVal, true),
+    ("DefnVal", TheDefnVal, true),
+    ("DeclVar", TheDeclVar, true),
+    ("DefnVar", TheDefnVar, true),
+    ("DeclDef", TheDeclDef, true),
+    ("DefnDef", TheDefnDef, false)
   )
 
 
-  forAll(RequiresStatementEndScenarios) { case (desc: String, term: Term, expectedResult: Boolean) =>
-    test(s"requiresStatementEnd($desc) should be $expectedResult") {
-      requiresStatementEnd(term) shouldBe expectedResult
+  forAll(RequiresEndDelimiterScenarios) { case (desc: String, stat: Stat, expectedResult: Boolean) =>
+    test(s"requiresEndDelimiter($desc) should be $expectedResult") {
+      requiresEndDelimiter(stat) shouldBe expectedResult
     }
   }
 }
