ef378a8b72ce4330bb0ebdc3e0e23b79a3669bcd
Effi Ban <effi.ban@kenshoo.com>
2022-09-16 00:22:56 +0300

    Support function types with more than 2 params using JOOL

--- src/main/scala/effiban/scala2java/transformers/ScalaToJavaFunctionTypeTransformer.scala
+++ src/main/scala/effiban/scala2java/transformers/ScalaToJavaFunctionTypeTransformer.scala
@@ -3,20 +3,21 @@ package effiban.scala2java.transformers
 import scala.meta.Type
 
 trait ScalaToJavaFunctionTypeTransformer {
-  def transform(functionType: Type.Function): Option[Type.Apply]
+  def transform(functionType: Type.Function): Type.Apply
 }
 
 object ScalaToJavaFunctionTypeTransformer extends ScalaToJavaFunctionTypeTransformer {
 
   // Transform a Scala function type (e.g.: Int => String) into a Java function type (e.g. Function<Int, String>)
-  override def transform(functionType: Type.Function): Option[Type.Apply] = {
+  override def transform(functionType: Type.Function): Type.Apply = {
     functionType.params match {
-      case Nil => Some(Type.Apply(Type.Name("Supplier"), List(functionType.res)))
-      case param :: Nil => Some(Type.Apply(Type.Name("Function"), List(param, functionType.res)))
-      case param1 :: param2 :: Nil => Some(Type.Apply(Type.Name("BiFunction"), List(param1, param2, functionType.res)))
-      //TODO - handle this with @FunctionalInterface or JOOL
-      case params => None
+      case Nil => Type.Apply(Type.Name("Supplier"), List(functionType.res))
+      case param :: Nil => Type.Apply(Type.Name("Function"), List(param, functionType.res))
+      case param1 :: param2 :: Nil => Type.Apply(Type.Name("BiFunction"), List(param1, param2, functionType.res))
+      // Since Java doesn't have built-in support for a function type of more than 2 params, using JOOL instead
+      case params => Type.Apply(Type.Name(joolFunctionTypeOf(params)), params :+ functionType.res)
     }
-
   }
+
+  private def joolFunctionTypeOf(params: List[Type]) = s"Function${params.length}"
 }
