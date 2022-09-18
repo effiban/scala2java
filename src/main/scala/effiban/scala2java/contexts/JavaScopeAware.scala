40a8110db44b9b3b44b83c7848df0182a17e137d
Effi Ban <effi.ban@kenshoo.com>
2022-09-18 22:40:47 +0300

    Add JavaScopeModifier

--- src/main/scala/effiban/scala2java/contexts/JavaScopeAware.scala
+++ src/main/scala/effiban/scala2java/contexts/JavaScopeAware.scala
@@ -1,7 +1,9 @@
 package effiban.scala2java.contexts
 
+import effiban.scala2java.entities.JavaScopeModifier.JavaScopeModifier
 import effiban.scala2java.entities.JavaTreeType.JavaTreeType
 
 trait JavaScopeAware {
   val javaScope: JavaTreeType
+  val javaScopeModifiers: Set[JavaScopeModifier] = Set.empty
 }
--- src/main/scala/effiban/scala2java/contexts/TemplateChildContext.scala
+++ src/main/scala/effiban/scala2java/contexts/TemplateChildContext.scala
@@ -1,10 +1,12 @@
 package effiban.scala2java.contexts
 
+import effiban.scala2java.entities.JavaScopeModifier.JavaScopeModifier
 import effiban.scala2java.entities.JavaTreeType.JavaTreeType
 
 import scala.meta.{Init, Term, Type}
 
 case class TemplateChildContext(override val javaScope: JavaTreeType,
+                                override val javaScopeModifiers: Set[JavaScopeModifier] = Set.empty,
                                 maybeClassName: Option[Type.Name] = None,
                                 inits: List[Init] = Nil,
                                 ctorTerms: List[Term] = Nil) extends JavaScopeAware
