3b37a05a9835d34a28853de2a4e174c2091010f6
Effi Ban <effi.ban@kenshoo.com>
2022-09-21 23:48:33 +0300

    Add ModsClassifier.areSealed()

--- src/test/scala/effiban/scala2java/classifiers/ModsClassifierTest.scala
+++ src/test/scala/effiban/scala2java/classifiers/ModsClassifierTest.scala
@@ -1,5 +1,6 @@
 package effiban.scala2java.classifiers
 
+import effiban.scala2java.classifiers.ModsClassifier.{arePublic, areSealed}
 import effiban.scala2java.testsuites.UnitTestSuite
 
 import scala.meta.{Mod, Name}
@@ -12,7 +13,7 @@ class ModsClassifierTest extends UnitTestSuite {
   private val ProtectedAnonymous = Mod.Protected(Name.Anonymous())
   private val ProtectedSpecific = Mod.Protected(Name("abc"))
 
-  private val Scenarios = Table[List[Mod], Boolean](
+  private val ArePublicScenarios = Table[List[Mod], Boolean](
     ("Mods", "ExpectedResult"),
     (List(PrivateAnonymous), false),
     (List(PrivateSpecific), false),
@@ -25,9 +26,23 @@ class ModsClassifierTest extends UnitTestSuite {
     (List.empty[Mod], true),
   )
 
-  forAll(Scenarios) { case (mods: List[Mod], expectedResult: Boolean) =>
+  private val AreSealedScenarios = Table[List[Mod], Boolean](
+    ("Mods", "ExpectedResult"),
+    (List(Mod.Sealed()), true),
+    (List(Mod.Sealed(), PrivateAnonymous), true),
+    (List(PrivateAnonymous), false),
+    (List.empty[Mod], false),
+  )
+
+  forAll(ArePublicScenarios) { case (mods: List[Mod], expectedResult: Boolean) =>
     test(s"The mods $mods should be considered '${if (expectedResult) "public" else "non-public"}'") {
-      ModsClassifier.arePublic(mods) shouldBe expectedResult
+      arePublic(mods) shouldBe expectedResult
+    }
+  }
+
+  forAll(AreSealedScenarios) { case (mods: List[Mod], expectedResult: Boolean) =>
+    test(s"The mods $mods should be considered '${if (expectedResult) "sealed" else "not sealed"}'") {
+      areSealed(mods) shouldBe expectedResult
     }
   }
 }
