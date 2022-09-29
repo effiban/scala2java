ce492b515f38685674ed7aadfd30eefab0c4f390
Effi Ban <effi.ban@kenshoo.com>
2022-09-29 23:48:45 +0300

    Transform Future(...) argument to a lambda

--- src/integrationTest/resources/testfiles/DefnVals/WithoutDeclaredType/InClass/Inferrable/TermApply/WithTermName/Future/Sample.java
+++ src/integrationTest/resources/testfiles/DefnVals/WithoutDeclaredType/InClass/Inferrable/TermApply/WithTermName/Future/Sample.java
@@ -2,7 +2,7 @@ package dummy;
 
 
 public class Sample {
-    public final CompletableFuture<String> x = CompletableFuture.supplyAsync("abc");
+    public final CompletableFuture<String> x = CompletableFuture.supplyAsync(() -> "abc");
 
     public Sample() {
     }
--- src/main/scala/effiban/scala2java/transformers/TermApplyTransformer.scala
+++ src/main/scala/effiban/scala2java/transformers/TermApplyTransformer.scala
@@ -13,10 +13,16 @@ class TermApplyTransformerImpl(termApplyNameTransformer: TermApplyNameTransforme
   @tailrec
   override final def transform(termApply: Term.Apply): Term.Apply = {
     termApply match {
+      case Term.Apply(future@Term.Name("Future"), List(arg)) => Term.Apply(transformName(future), List(Term.Function(Nil, arg)))
+      case Term.Apply(Term.ApplyType(future@Term.Name("Future"), types), List(arg)) =>
+        Term.Apply(Term.ApplyType(transformName(future), types), List(Term.Function(Nil, arg)))
+
       case Term.Apply(name : Term.Name, args) => Term.Apply(transformName(name), args)
       case Term.Apply(Term.ApplyType(name: Term.Name, types), args) => Term.Apply(Term.ApplyType(transformName(name), types), args)
+
       // Invocation of method with more than one param list
       case Term.Apply(Term.Apply(fun, args1), args2) => transform(Term.Apply(fun, args1 ++ args2))
+
       case other => other
     }
   }
