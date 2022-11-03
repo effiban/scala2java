6186befbbb5596f920989cba0cfa1d43c9d0c7c3
Effi Ban <effi.ban@kenshoo.com>
2022-11-03 23:13:00 +0200

    Add comment for implicit

---
+++ src/integrationTest/resources/testfiles/DeclDefs/WithTwoParamLists/WithImplicits/Sample.java
@@ -0,0 +1,10 @@
+package dummy;
+
+
+public interface Sample {
+
+    void foo(final String param1,
+        final int param2,
+        /* implicit */final String param3,
+        /* implicit */final int param4);
+}
--- src/main/scala/io/github/effiban/scala2java/traversers/ModListTraverser.scala
+++ src/main/scala/io/github/effiban/scala2java/traversers/ModListTraverser.scala
@@ -4,6 +4,9 @@ import io.github.effiban.scala2java.contexts.JavaModifiersContext
 import io.github.effiban.scala2java.resolvers.JavaModifiersResolver
 import io.github.effiban.scala2java.writers.JavaWriter
 
+import scala.meta.Mod
+import scala.meta.Mod.Implicit
+
 trait ModListTraverser {
   def traverse(javaModifiersContext: JavaModifiersContext, annotsOnSameLine: Boolean = false): Unit
 }
@@ -16,6 +19,13 @@ class ModListTraverserImpl(annotListTraverser: => AnnotListTraverser,
 
   override def traverse(javaModifiersContext: JavaModifiersContext, annotsOnSameLine: Boolean = false): Unit = {
     annotListTraverser.traverseMods(javaModifiersContext.scalaMods, annotsOnSameLine)
+    handleImplicitIfExists(javaModifiersContext.scalaMods)
     writeModifiers(javaModifiersResolver.resolve(javaModifiersContext))
+  }
+
+  private def handleImplicitIfExists(scalaMods: List[Mod]): Unit = {
+    if (scalaMods.exists(_.isInstanceOf[Implicit])) {
+      writeComment("implicit")
+    }
   }
 }
