1d5f4ff33f32860d2ba6fdb4887f8f923d90885d
Effi Ban <effi.ban@kenshoo.com>
2023-04-15 22:25:24 +0300

    Extract `BindRenderer`

--- scala2java-core/src/main/scala/io/github/effiban/scala2java/core/traversers/BindTraverser.scala
+++ scala2java-core/src/main/scala/io/github/effiban/scala2java/core/traversers/BindTraverser.scala
@@ -1,20 +1,17 @@
 package io.github.effiban.scala2java.core.traversers
 
-import io.github.effiban.scala2java.core.writers.JavaWriter
+import io.github.effiban.scala2java.core.renderers.BindRenderer
 
 import scala.meta.Pat
 import scala.meta.Pat.Bind
 
 trait BindTraverser extends ScalaTreeTraverser[Pat.Bind]
 
-private[traversers] class BindTraverserImpl(patTraverser: => PatTraverser)
-                                           (implicit javaWriter: JavaWriter) extends BindTraverser {
-
-  import javaWriter._
+private[traversers] class BindTraverserImpl(bindRenderer: BindRenderer) extends BindTraverser {
 
   // Pattern match bind variable, e.g.: a @ A().
   override def traverse(patternBind: Bind): Unit = {
     //TODO - consider supporting in Java by converting to a guard?
-    writeComment(patternBind.toString())
+    bindRenderer.render(patternBind)
   }
 }
