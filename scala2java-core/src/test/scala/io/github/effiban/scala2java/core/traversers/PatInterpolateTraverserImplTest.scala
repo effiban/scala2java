98033c84d7e93e499ce553e64bd361f987263c91
Effi Ban <effi.ban@kenshoo.com>
2023-04-22 07:58:46 +0300

    Separate `PatInterpolateTraverser` and `PatInterpolateRenderer`

--- scala2java-core/src/test/scala/io/github/effiban/scala2java/core/traversers/PatInterpolateTraverserImplTest.scala
+++ scala2java-core/src/test/scala/io/github/effiban/scala2java/core/traversers/PatInterpolateTraverserImplTest.scala
@@ -1,15 +1,10 @@
 package io.github.effiban.scala2java.core.traversers
 
-import io.github.effiban.scala2java.core.renderers.PatInterpolateRenderer
 import io.github.effiban.scala2java.core.testsuites.UnitTestSuite
-import io.github.effiban.scala2java.test.utils.matchers.TreeMatcher.eqTree
 
 import scala.meta.{Lit, Pat, Term}
 
 class PatInterpolateTraverserImplTest extends UnitTestSuite {
-
-  private val patInterpolateRenderer = mock[PatInterpolateRenderer]
-  private val patInterpolateTraverser = new PatInterpolateTraverserImpl(patInterpolateRenderer)
 
   test("traverse") {
     val patInterpolate = Pat.Interpolate(
@@ -18,9 +13,7 @@ class PatInterpolateTraverserImplTest extends UnitTestSuite {
       args = List(Term.Name("name"))
     )
 
-    patInterpolateTraverser.traverse(patInterpolate)
-
-    verify(patInterpolateRenderer).render(eqTree(patInterpolate))
+    PatInterpolateTraverser.traverse(patInterpolate).structure shouldBe patInterpolate.structure
   }
 
 }
