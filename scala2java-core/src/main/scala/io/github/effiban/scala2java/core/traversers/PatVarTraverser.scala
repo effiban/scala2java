fe20ae630233494570f6ac3165df9fc257930432
Effi Ban <effi.ban@kenshoo.com>
2023-04-22 14:12:49 +0300

    In `PatVarTraverser` use `TermNameRenderer` instead of `TermNameTraverser`

--- scala2java-core/src/main/scala/io/github/effiban/scala2java/core/traversers/PatVarTraverser.scala
+++ scala2java-core/src/main/scala/io/github/effiban/scala2java/core/traversers/PatVarTraverser.scala
@@ -1,13 +1,15 @@
 package io.github.effiban.scala2java.core.traversers
 
+import io.github.effiban.scala2java.core.renderers.TermNameRenderer
+
 import scala.meta.Pat
 
 trait PatVarTraverser extends ScalaTreeTraverser[Pat.Var]
 
-private[traversers] class PatVarTraverserImpl(defaultTermNameTraverser: => TermNameTraverser) extends PatVarTraverser {
+private[traversers] class PatVarTraverserImpl(termNameRenderer: TermNameRenderer) extends PatVarTraverser {
 
   // Pattern match variable, e.g. `a` in case a =>
   override def traverse(patternVar: Pat.Var): Unit = {
-    defaultTermNameTraverser.traverse(patternVar.name)
+    termNameRenderer.render(patternVar.name)
   }
 }
--- scala2java-core/src/test/scala/io/github/effiban/scala2java/core/traversers/PatVarTraverserImplTest.scala
+++ scala2java-core/src/test/scala/io/github/effiban/scala2java/core/traversers/PatVarTraverserImplTest.scala
@@ -1,5 +1,6 @@
 package io.github.effiban.scala2java.core.traversers
 
+import io.github.effiban.scala2java.core.renderers.TermNameRenderer
 import io.github.effiban.scala2java.core.testsuites.UnitTestSuite
 import io.github.effiban.scala2java.test.utils.matchers.TreeMatcher.eqTree
 
@@ -7,15 +8,15 @@ import scala.meta.{Pat, Term}
 
 class PatVarTraverserImplTest extends UnitTestSuite {
 
-  private val defaultTermNameTraverser = mock[TermNameTraverser]
+  private val termNameRenderer = mock[TermNameRenderer]
 
-  private val patVarTraverser = new PatVarTraverserImpl(defaultTermNameTraverser)
+  private val patVarTraverser = new PatVarTraverserImpl(termNameRenderer)
 
   test("traverse()") {
     val termName = Term.Name("x")
 
     patVarTraverser.traverse(Pat.Var(termName))
 
-    verify(defaultTermNameTraverser).traverse(eqTree(termName))
+    verify(termNameRenderer).render(eqTree(termName))
   }
 }
