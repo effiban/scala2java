9e7fd06e075de3f861d175d807df73adcebda96a
Effi Ban <effi.ban@kenshoo.com>
2023-05-07 21:56:35 +0300

    Separate `ClassOfTraverser` and `ClassOfRenderer`

--- scala2java-core/src/main/scala/io/github/effiban/scala2java/core/traversers/MainApplyTypeTraverserImpl.scala
+++ scala2java-core/src/main/scala/io/github/effiban/scala2java/core/traversers/MainApplyTypeTraverserImpl.scala
@@ -1,6 +1,7 @@
 package io.github.effiban.scala2java.core.traversers
 
 import io.github.effiban.scala2java.core.entities.TermNameValues.ScalaClassOf
+import io.github.effiban.scala2java.core.renderers.ClassOfRenderer
 
 import scala.meta.Term
 import scala.meta.Term.ApplyType
@@ -8,12 +9,15 @@ import scala.meta.Term.ApplyType
 trait MainApplyTypeTraverser extends ScalaTreeTraverser[Term.ApplyType]
 
 private[traversers] class MainApplyTypeTraverserImpl(classOfTraverser: => ClassOfTraverser,
+                                                     classOfRenderer: => ClassOfRenderer,
                                                      standardApplyTypeTraverser: => StandardApplyTypeTraverser)
   extends MainApplyTypeTraverser {
 
   // parametrized type application, e.g.: classOf[X], identity[X]
   override def traverse(termApplyType: ApplyType): Unit = termApplyType.fun match {
-    case Term.Name(ScalaClassOf) => classOfTraverser.traverse(termApplyType)
+    case Term.Name(ScalaClassOf) =>
+      val traversedClassOf = classOfTraverser.traverse(termApplyType)
+      classOfRenderer.render(traversedClassOf)
     case _ => standardApplyTypeTraverser.traverse(termApplyType)
   }
 }
--- scala2java-core/src/test/scala/io/github/effiban/scala2java/core/traversers/MainApplyTypeTraverserImplTest.scala
+++ scala2java-core/src/test/scala/io/github/effiban/scala2java/core/traversers/MainApplyTypeTraverserImplTest.scala
@@ -1,29 +1,33 @@
 package io.github.effiban.scala2java.core.traversers
 
+import io.github.effiban.scala2java.core.renderers.ClassOfRenderer
 import io.github.effiban.scala2java.core.testsuites.UnitTestSuite
-import io.github.effiban.scala2java.core.testtrees.TermNames.ScalaClassOf
 import io.github.effiban.scala2java.test.utils.matchers.TreeMatcher.eqTree
 
-import scala.meta.{Term, XtensionQuasiquoteTerm, XtensionQuasiquoteType}
+import scala.meta.XtensionQuasiquoteTerm
 
 class MainApplyTypeTraverserImplTest extends UnitTestSuite {
 
   private val classOfTraverser = mock[ClassOfTraverser]
+  private val classOfRenderer = mock[ClassOfRenderer]
   private val standardApplyTypeTraverser = mock[StandardApplyTypeTraverser]
 
   private val mainApplyTypeTraverser = new MainApplyTypeTraverserImpl(
     classOfTraverser,
+    classOfRenderer,
     standardApplyTypeTraverser
   )
 
 
   test("traverse() when function is 'classOf' should call the dedicated traverser") {
-    val typeName = t"T"
-    val classOfType = Term.ApplyType(fun = ScalaClassOf, targs = List(typeName))
+    val classOfType = q"classOf[T]"
+    val traversedClassOfType = q"classOf[U]"
+
+    doReturn(traversedClassOfType).when(classOfTraverser).traverse(eqTree(classOfType))
 
     mainApplyTypeTraverser.traverse(classOfType)
 
-    verify(classOfTraverser).traverse(eqTree(classOfType))
+    verify(classOfRenderer).render(eqTree(traversedClassOfType))
   }
 
   test("traverse() when function is 'foo', should call the standard traverser") {
