050e27b3fb94a9c0e5029331a8dbe227c4595fee
effiban <effi.ban@skai.io>
2023-05-25 23:56:15 +0300

    Add and call `DeclDefDesugarer`

--- scala2java-core/src/main/scala/io/github/effiban/scala2java/core/desugarers/DeclDesugarer.scala
+++ scala2java-core/src/main/scala/io/github/effiban/scala2java/core/desugarers/DeclDesugarer.scala
@@ -4,10 +4,10 @@ import scala.meta.Decl
 
 trait DeclDesugarer extends SameTypeDesugarer[Decl]
 
-private[desugarers] class DeclDesugarerImpl() extends DeclDesugarer {
+private[desugarers] class DeclDesugarerImpl(declDefDesugarer: => DeclDefDesugarer) extends DeclDesugarer {
 
   override def desugar(decl: Decl): Decl = decl match {
-    case declDef: Decl.Def => declDef // TODO
+    case declDef: Decl.Def => declDefDesugarer.desugar(declDef)
     case other => other
   }
 
