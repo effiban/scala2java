008d67cac76a7fc8fb477fd8c0831f65a5ea2139
effiban <effi.ban@skai.io>
2023-06-20 00:25:34 +0300

    Extract `BlockLastStatTraverser` from `BlockStatTraverser`

--- scala2java-core/src/main/scala/io/github/effiban/scala2java/core/traversers/BlockStatTraverser.scala
+++ scala2java-core/src/main/scala/io/github/effiban/scala2java/core/traversers/BlockStatTraverser.scala
@@ -1,22 +1,11 @@
 package io.github.effiban.scala2java.core.traversers
 
-import io.github.effiban.scala2java.core.entities.Decision.{Decision, No, Uncertain, Yes}
-import io.github.effiban.scala2java.core.resolvers.ShouldReturnValueResolver
-import io.github.effiban.scala2java.core.traversers.results.BlockStatTraversalResult
-
-import scala.meta.Term.{If, Return, Try, TryWithHandler}
 import scala.meta.{Decl, Defn, Stat, Term}
 
-trait BlockStatTraverser {
-
-  def traverse(stat: Stat): Stat
-
-  def traverseLast(stat: Stat, shouldReturnValue: Decision = No): BlockStatTraversalResult
-}
+trait BlockStatTraverser extends ScalaTreeTraverser1[Stat]
 
 private[traversers] class BlockStatTraverserImpl(expressionTermRefTraverser: => ExpressionTermRefTraverser,
-                                                 defaultTermTraverser: => DefaultTermTraverser,
-                                                 shouldReturnValueResolver: => ShouldReturnValueResolver) extends BlockStatTraverser {
+                                                 defaultTermTraverser: => DefaultTermTraverser) extends BlockStatTraverser {
 
   override def traverse(stat: Stat): Stat = stat match {
     case termRef: Term.Ref => expressionTermRefTraverser.traverse(termRef)
@@ -26,24 +15,5 @@ private[traversers] class BlockStatTraverserImpl(expressionTermRefTraverser: => 
     case declVar: Decl.Var => declVar //TODO
     // TODO support other stats once renderers are ready
     case aStat: Stat => throw new UnsupportedOperationException(s"Rendering of $aStat in a block is not supported yet")
-  }
-
-  override def traverseLast(stat: Stat, shouldReturnValue: Decision = No): BlockStatTraversalResult = {
-    stat match {
-      case `if`: If => BlockStatTraversalResult(`if`)// TODO
-      case `try`: Try => BlockStatTraversalResult(`try`) // TODO
-      case tryWithHandler: TryWithHandler => BlockStatTraversalResult(tryWithHandler) //TODO
-      case term: Term => traverseLastTerm(term, shouldReturnValue)
-      case _ => BlockStatTraversalResult(traverse(stat))
-    }
-  }
-
-  private def traverseLastTerm(term: Term, shouldReturnValue: Decision): BlockStatTraversalResult = {
-    val shouldTermReturnValue = shouldReturnValueResolver.resolve(term, shouldReturnValue)
-    shouldTermReturnValue match {
-      case Yes => BlockStatTraversalResult(traverse(Return(term)))
-      case Uncertain => BlockStatTraversalResult(traverse(term), uncertainReturn = true)
-      case No => BlockStatTraversalResult(traverse(term))
-    }
   }
 }
--- scala2java-core/src/test/scala/io/github/effiban/scala2java/core/traversers/BlockStatTraverserImplTest.scala
+++ scala2java-core/src/test/scala/io/github/effiban/scala2java/core/traversers/BlockStatTraverserImplTest.scala
@@ -1,14 +1,8 @@
 package io.github.effiban.scala2java.core.traversers
 
-import io.github.effiban.scala2java.core.entities.Decision.{No, Uncertain, Yes}
-import io.github.effiban.scala2java.core.matchers.BlockStatTraversalResultScalatestMatcher.equalBlockStatTraversalResult
-import io.github.effiban.scala2java.core.resolvers.ShouldReturnValueResolver
 import io.github.effiban.scala2java.core.testsuites.UnitTestSuite
-import io.github.effiban.scala2java.core.traversers.results.BlockStatTraversalResult
 import io.github.effiban.scala2java.test.utils.matchers.TreeMatcher.eqTree
-import org.mockito.ArgumentMatchersSugar.eqTo
 
-import scala.meta.Term.Return
 import scala.meta.XtensionQuasiquoteTerm
 
 class BlockStatTraverserImplTest extends UnitTestSuite {
@@ -21,12 +15,10 @@ class BlockStatTraverserImplTest extends UnitTestSuite {
   
   private val expressionTermRefTraverser = mock[ExpressionTermRefTraverser]
   private val defaultTermTraverser = mock[DefaultTermTraverser]
-  private val shouldReturnValueResolver = mock[ShouldReturnValueResolver]
 
   private val blockStatTraverser = new BlockStatTraverserImpl(
     expressionTermRefTraverser,
-    defaultTermTraverser,
-    shouldReturnValueResolver
+    defaultTermTraverser
   )
 
 
@@ -41,41 +33,5 @@ class BlockStatTraverserImplTest extends UnitTestSuite {
 
     blockStatTraverser.traverse(TheTermApply).structure shouldBe TheTraversedTermApply.structure
   }
-  
-  test("traverseLast() for a 'Term.Apply' when shouldReturnValue=Yes and shouldTermReturnValue=Yes") {
-    val expectedTraversalResult = BlockStatTraversalResult(Return(TheTraversedTermApply))
-    
-    when(shouldReturnValueResolver.resolve(eqTree(TheTermApply), eqTo(Yes))).thenReturn(Yes)
-    doReturn(Return(TheTraversedTermApply)).when(defaultTermTraverser).traverse(eqTree(Return(TheTermApply)))
-
-    blockStatTraverser.traverseLast(TheTermApply, shouldReturnValue = Yes) should equalBlockStatTraversalResult(expectedTraversalResult)
-  }
-
-  test("traverseLast() for a 'Term.Apply' when shouldReturnValue=Yes and shouldTermReturnValue=No") {
-    val expectedTraversalResult = BlockStatTraversalResult(TheTraversedTermApply)
-
-    when(shouldReturnValueResolver.resolve(eqTree(TheTermApply), eqTo(Yes))).thenReturn(No)
-    doReturn(TheTraversedTermApply).when(defaultTermTraverser).traverse(eqTree(TheTermApply))
-
-    blockStatTraverser.traverseLast(TheTermApply, shouldReturnValue = Yes) should equalBlockStatTraversalResult(expectedTraversalResult)
-  }
-
-  test("traverseLast() for a 'Term.Apply' when shouldReturnValue=Uncertain and shouldTermReturnValue=Uncertain") {
-    val expectedTraversalResult = BlockStatTraversalResult(TheTraversedTermApply, uncertainReturn = true)
-
-    when(shouldReturnValueResolver.resolve(eqTree(TheTermApply), eqTo(Uncertain))).thenReturn(Uncertain)
-    doReturn(TheTraversedTermApply).when(defaultTermTraverser).traverse(eqTree(TheTermApply))
-
-    blockStatTraverser.traverseLast(TheTermApply, shouldReturnValue = Uncertain) should
-      equalBlockStatTraversalResult(expectedTraversalResult)
-  }
-
-  test("traverseLast() for a 'Term.Apply' when shouldReturnValue=No and shouldTermReturnValue=No") {
-    val expectedTraversalResult = BlockStatTraversalResult(TheTraversedTermApply)
-
-    when(shouldReturnValueResolver.resolve(eqTree(TheTermApply), eqTo(No))).thenReturn(No)
-    doReturn(TheTraversedTermApply).when(defaultTermTraverser).traverse(eqTree(TheTermApply))
-
-    blockStatTraverser.traverseLast(TheTermApply, shouldReturnValue = No) should equalBlockStatTraversalResult(expectedTraversalResult)
-  }
 }
+
