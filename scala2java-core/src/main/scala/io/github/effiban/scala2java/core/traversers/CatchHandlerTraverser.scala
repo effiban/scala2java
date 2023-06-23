ad76082665752742274d22a64a01b9d1cc474b8a
effiban <effi.ban@skai.io>
2023-06-23 18:07:31 +0300

    Add `CatchHandlerTraverser`

---
+++ scala2java-core/src/main/scala/io/github/effiban/scala2java/core/traversers/CatchHandlerTraverser.scala
@@ -0,0 +1,30 @@
+package io.github.effiban.scala2java.core.traversers
+
+import io.github.effiban.scala2java.core.contexts.{BlockContext, CatchHandlerContext}
+import io.github.effiban.scala2java.core.traversers.results.CatchHandlerTraversalResult
+
+import scala.meta.Case
+
+trait CatchHandlerTraverser {
+  def traverse(catchCase: Case,
+               context: CatchHandlerContext = CatchHandlerContext()): CatchHandlerTraversalResult
+}
+
+private[traversers] class CatchHandlerTraverserImpl(catchArgumentTraverser: => CatchArgumentTraverser,
+                                                    blockWrappingTermTraverser: BlockWrappingTermTraverser) extends CatchHandlerTraverser {
+
+  override def traverse(catchCase: Case,
+                        context: CatchHandlerContext = CatchHandlerContext()): CatchHandlerTraversalResult = {
+    val traversedPat = catchArgumentTraverser.traverse(catchCase.pat)
+    // TODO - handle cond by moving into body
+    val bodyTraversalResult = blockWrappingTermTraverser.traverse(catchCase.body,
+      context = BlockContext(shouldReturnValue = context.shouldReturnValue))
+    val traversedCatchCase = Case(
+      pat = traversedPat,
+      cond = None,
+      body = bodyTraversalResult.block
+    )
+
+    CatchHandlerTraversalResult(catchCase = traversedCatchCase, uncertainReturn = bodyTraversalResult.uncertainReturn)
+  }
+}
---
+++ scala2java-core/src/test/scala/io/github/effiban/scala2java/core/traversers/CatchHandlerTraverserImplTest.scala
@@ -0,0 +1,90 @@
+package io.github.effiban.scala2java.core.traversers
+
+import io.github.effiban.scala2java.core.contexts.{BlockContext, CatchHandlerContext}
+import io.github.effiban.scala2java.core.entities.Decision.{Uncertain, Yes}
+import io.github.effiban.scala2java.core.matchers.BlockContextMatcher.eqBlockContext
+import io.github.effiban.scala2java.core.matchers.CatchHandlerTraversalResultScalatestMatcher.equalCatchHandlerTraversalResult
+import io.github.effiban.scala2java.core.testsuites.UnitTestSuite
+import io.github.effiban.scala2java.core.traversers.results.{BlockTraversalResult, CatchHandlerTraversalResult}
+import io.github.effiban.scala2java.test.utils.matchers.TreeMatcher.eqTree
+
+import scala.meta.{Case, XtensionQuasiquoteCaseOrPattern, XtensionQuasiquoteTerm}
+
+class CatchHandlerTraverserImplTest extends UnitTestSuite {
+
+  private val CatchArg = p"e"
+  private val TraversedCatchArg = p"e: RuntimeException"
+
+  private val Body = q"log.error(e)"
+  private val TraversedBody =
+    q"""
+    {
+      log.error(ee)
+    }
+    """
+
+  private val CatchCase = Case(
+    pat = CatchArg,
+    cond = None,
+    body = Body
+  )
+  private val TraversedCatchCase = Case(
+    pat = TraversedCatchArg,
+    cond = None,
+    body = TraversedBody
+  )
+
+  private val catchArgumentTraverser = mock[CatchArgumentTraverser]
+  private val blockWrappingTermTraverser = mock[BlockWrappingTermTraverser]
+
+  private val catchHandlerTraverser = new CatchHandlerTraverserImpl(
+    catchArgumentTraverser,
+    blockWrappingTermTraverser
+  )
+
+  test("traverse() when shouldReturnValue=No") {
+    val expectedBlockTraversalResult = BlockTraversalResult(TraversedBody)
+    val expectedCatchHandlerTraversalResult = CatchHandlerTraversalResult(TraversedCatchCase)
+
+    doReturn(TraversedCatchArg).when(catchArgumentTraverser).traverse(eqTree(CatchArg))
+
+    doReturn(expectedBlockTraversalResult).when(blockWrappingTermTraverser).traverse(
+      term = eqTree(Body),
+      context = eqBlockContext(BlockContext())
+    )
+
+    catchHandlerTraverser.traverse(CatchCase) should equalCatchHandlerTraversalResult(expectedCatchHandlerTraversalResult)
+  }
+
+  test("traverse() when shouldReturnValue=Yes") {
+    val catchHandlerContext = CatchHandlerContext(shouldReturnValue = Yes)
+    val expectedBlockContext = BlockContext(shouldReturnValue = Yes)
+
+    val expectedBlockTraversalResult = BlockTraversalResult(TraversedBody)
+    val expectedCatchHandlerTraversalResult = CatchHandlerTraversalResult(TraversedCatchCase)
+
+    doReturn(TraversedCatchArg).when(catchArgumentTraverser).traverse(eqTree(CatchArg))
+
+    doReturn(expectedBlockTraversalResult)
+      .when(blockWrappingTermTraverser).traverse(term = eqTree(Body), context = eqBlockContext(expectedBlockContext))
+
+    val actualCatchHandlerResult = catchHandlerTraverser.traverse(CatchCase, catchHandlerContext)
+    actualCatchHandlerResult should equalCatchHandlerTraversalResult(expectedCatchHandlerTraversalResult)
+  }
+
+  test("traverse() when shouldReturnValue=Uncertain and output uncertainReturn=true") {
+    val catchHandlerContext = CatchHandlerContext(shouldReturnValue = Uncertain)
+    val expectedBlockContext = BlockContext(shouldReturnValue = Uncertain)
+
+    val expectedBlockTraversalResult = BlockTraversalResult(TraversedBody, uncertainReturn = true)
+    val expectedCatchHandlerTraversalResult = CatchHandlerTraversalResult(TraversedCatchCase, uncertainReturn = true)
+
+    doReturn(TraversedCatchArg).when(catchArgumentTraverser).traverse(eqTree(CatchArg))
+
+    doReturn(expectedBlockTraversalResult)
+      .when(blockWrappingTermTraverser).traverse(term = eqTree(Body), context = eqBlockContext(expectedBlockContext))
+
+    val actualCatchHandlerResult = catchHandlerTraverser.traverse(CatchCase, catchHandlerContext)
+    actualCatchHandlerResult should equalCatchHandlerTraversalResult(expectedCatchHandlerTraversalResult)
+  }
+}
