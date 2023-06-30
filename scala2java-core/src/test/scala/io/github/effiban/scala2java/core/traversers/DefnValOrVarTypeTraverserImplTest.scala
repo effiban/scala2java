0ac63fa09844bd6d415b126aadeaba4acf1bc45a
effiban <effi.ban@skai.io>
2023-07-01 02:12:07 +0300

    Add `DefnValOrVarTypeTraverser`

---
+++ scala2java-core/src/test/scala/io/github/effiban/scala2java/core/traversers/DefnValOrVarTypeTraverserImplTest.scala
@@ -0,0 +1,52 @@
+package io.github.effiban.scala2java.core.traversers
+
+import io.github.effiban.scala2java.core.testsuites.UnitTestSuite
+import io.github.effiban.scala2java.core.typeinference.TermTypeInferrer
+import io.github.effiban.scala2java.test.utils.matchers.TreeMatcher.eqTree
+
+import scala.meta.{XtensionQuasiquoteTerm, XtensionQuasiquoteType}
+
+class DefnValOrVarTypeTraverserImplTest extends UnitTestSuite {
+
+  private val typeTraverser = mock[TypeTraverser]
+  private val termTypeInferrer = mock[TermTypeInferrer]
+
+  private val defnValOrVarTypeTraverser = new DefnValOrVarTypeTraverserImpl(
+    typeTraverser,
+    termTypeInferrer
+  )
+
+  test("traverse when has declared type should traverse it") {
+    val tpe = t"MyType"
+    val traversedType = t"MyTraversedType"
+
+    doReturn(traversedType).when(typeTraverser).traverse(eqTree(tpe))
+
+    val maybeOutputType = defnValOrVarTypeTraverser.traverse(maybeDeclType = Some(tpe))
+    maybeOutputType.value.structure shouldBe traversedType.structure
+  }
+
+  test("traverse when has no declared type, has RHS, and type is inferred - should traverse it") {
+    val rhs = q"myInstance"
+    val tpe = t"MyType"
+    val traversedType = t"MyTraversedType"
+
+    when(termTypeInferrer.infer(eqTree(rhs))).thenReturn(Some(tpe))
+    doReturn(traversedType).when(typeTraverser).traverse(eqTree(tpe))
+
+    val maybeOutputType = defnValOrVarTypeTraverser.traverse(maybeRhs = Some(rhs))
+    maybeOutputType.value.structure shouldBe traversedType.structure
+  }
+
+  test("traverse when has no declared type, has RHS, and type not inferred - should return None") {
+    val rhs = q"nonInferrable"
+
+    when(termTypeInferrer.infer(eqTree(rhs))).thenReturn(None)
+
+    defnValOrVarTypeTraverser.traverse(maybeRhs = Some(rhs)) shouldBe None
+  }
+
+  test("traverse when has no declared type and has no RHS - should return None") {
+    defnValOrVarTypeTraverser.traverse() shouldBe None
+  }
+}
