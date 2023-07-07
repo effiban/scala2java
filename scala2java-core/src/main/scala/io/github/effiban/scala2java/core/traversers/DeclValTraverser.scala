5b7a064970baca4e7a8a5fc576e8409550bff1ac
effiban <effi.ban@skai.io>
2023-07-07 15:12:54 +0300

    Migrate `DeclValTraverser` to use new `ModListTraverser` and renderer

--- scala2java-core/src/main/scala/io/github/effiban/scala2java/core/traversers/DeclValTraverser.scala
+++ scala2java-core/src/main/scala/io/github/effiban/scala2java/core/traversers/DeclValTraverser.scala
@@ -2,7 +2,8 @@ package io.github.effiban.scala2java.core.traversers
 
 import io.github.effiban.scala2java.core.contexts.{ModifiersContext, StatContext}
 import io.github.effiban.scala2java.core.entities.JavaTreeType
-import io.github.effiban.scala2java.core.renderers.{PatListRenderer, TypeRenderer}
+import io.github.effiban.scala2java.core.renderers.contextfactories.ModifiersRenderContextFactory
+import io.github.effiban.scala2java.core.renderers.{ModListRenderer, PatListRenderer, TypeRenderer}
 import io.github.effiban.scala2java.core.writers.JavaWriter
 
 import scala.meta.Decl
@@ -11,7 +12,9 @@ trait DeclValTraverser {
   def traverse(valDecl: Decl.Val, context: StatContext = StatContext()): Unit
 }
 
-private[traversers] class DeclValTraverserImpl(modListTraverser: => DeprecatedModListTraverser,
+private[traversers] class DeclValTraverserImpl(modListTraverser: => ModListTraverser,
+                                               modifiersRenderContextFactory: ModifiersRenderContextFactory,
+                                               modListRenderer: => ModListRenderer,
                                                typeTraverser: => TypeTraverser,
                                                typeRenderer: => TypeRenderer,
                                                patTraverser: => PatTraverser,
@@ -22,7 +25,9 @@ private[traversers] class DeclValTraverserImpl(modListTraverser: => DeprecatedMo
 
   //TODO replace interface data member declaration (invalid in Java) with method declaration
   override def traverse(valDecl: Decl.Val, context: StatContext = StatContext()): Unit = {
-    modListTraverser.traverse(ModifiersContext(valDecl, JavaTreeType.Variable, context.javaScope))
+    val modListTraversalResult = modListTraverser.traverse(ModifiersContext(valDecl, JavaTreeType.Variable, context.javaScope))
+    val modifiersRenderContext = modifiersRenderContextFactory(modListTraversalResult)
+    modListRenderer.render(modifiersRenderContext)
     val traversedType = typeTraverser.traverse(valDecl.decltpe)
     typeRenderer.render(traversedType)
     write(" ")
