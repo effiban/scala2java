709f0dfde7aa5dee4c2dfcdb09a773ef552aab01
effiban <effi.ban@skai.io>
2023-07-08 18:41:12 +0300

    Remove unused render contexts

--- scala2java-core/src/main/scala/io/github/effiban/scala2java/core/contexts/IfRenderContext.scala
+++
@@ -1,4 +0,0 @@
-package io.github.effiban.scala2java.core.contexts
-
-case class IfRenderContext(thenContext: BlockRenderContext = BlockRenderContext(),
-                           elseContext: BlockRenderContext = BlockRenderContext()) extends BlockStatRenderContext
--- scala2java-core/src/main/scala/io/github/effiban/scala2java/core/contexts/TryRenderContext.scala
+++
@@ -1,4 +0,0 @@
-package io.github.effiban.scala2java.core.contexts
-
-case class TryRenderContext(exprContext: BlockRenderContext = BlockRenderContext(),
-                            catchContexts: List[BlockRenderContext] = Nil) extends BlockStatRenderContext
