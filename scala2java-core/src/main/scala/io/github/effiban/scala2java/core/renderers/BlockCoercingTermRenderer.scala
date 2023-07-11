40ca0596af7f38a3bb6fe835ec9d0c518153802e
effiban <effi.ban@skai.io>
2023-07-11 18:19:08 +0300

    Add `DefnDefRenderer`

---
+++ scala2java-core/src/main/scala/io/github/effiban/scala2java/core/renderers/BlockCoercingTermRenderer.scala
@@ -0,0 +1,21 @@
+package io.github.effiban.scala2java.core.renderers
+
+import io.github.effiban.scala2java.core.contexts.BlockRenderContext
+
+import scala.meta.Term
+import scala.meta.Term.Block
+
+trait BlockCoercingTermRenderer {
+  def render(term: Term, context: BlockRenderContext = BlockRenderContext()): Unit
+}
+
+private[renderers] class BlockCoercingTermRendererImpl(blockRenderer: => BlockRenderer) extends BlockCoercingTermRenderer {
+
+  override def render(term: Term, context: BlockRenderContext = BlockRenderContext()): Unit = {
+    val block = term match {
+      case aBlock: Block => aBlock
+      case _ => throw new IllegalStateException(s"The term $term should have been converted to a Block before this point")
+    }
+    blockRenderer.render(block, context)
+  }
+}
---
+++ scala2java-core/src/main/scala/io/github/effiban/scala2java/core/renderers/DefnDefRenderer.scala
@@ -0,0 +1,61 @@
+package io.github.effiban.scala2java.core.renderers
+
+import io.github.effiban.scala2java.core.contexts._
+import io.github.effiban.scala2java.core.entities.TraversalConstants.UnknownType
+import io.github.effiban.scala2java.core.writers.JavaWriter
+
+import scala.meta.{Defn, Type}
+
+trait DefnDefRenderer {
+  def render(defnDef: Defn.Def, context: DefnDefRenderContext = DefnDefRenderContext()): Unit
+}
+
+private[renderers] class DefnDefRendererImpl(modListRenderer: => ModListRenderer,
+                                             typeParamListRenderer: => TypeParamListRenderer,
+                                             termNameRenderer: TermNameRenderer,
+                                             typeRenderer: => TypeRenderer,
+                                             termParamListRenderer: => TermParamListRenderer,
+                                             blockCoercingTermRenderer: => BlockCoercingTermRenderer)
+                                            (implicit javaWriter: JavaWriter) extends DefnDefRenderer {
+
+  import javaWriter._
+
+  override def render(defnDef: Defn.Def, context: DefnDefRenderContext = DefnDefRenderContext()): Unit = {
+    writeLine()
+    renderModifiers(defnDef, context)
+    renderTypeParams(defnDef)
+    renderMethodType(defnDef.decltpe)
+    termNameRenderer.render(defnDef.name)
+    renderMethodParamsAndBody(defnDef, context)
+  }
+
+  private def renderModifiers(defnDef: Defn.Def, context: DefnDefRenderContext): Unit = {
+    val modifiersRenderContext = ModifiersRenderContext(scalaMods = defnDef.mods, javaModifiers = context.methodJavaModifiers)
+    modListRenderer.render(modifiersRenderContext)
+  }
+
+  private def renderTypeParams(defnDef: Defn.Def): Unit = {
+    if (defnDef.tparams.nonEmpty) {
+      typeParamListRenderer.render(defnDef.tparams)
+      write(" ")
+    }
+  }
+
+  private def renderMethodType(maybeType: Option[Type]): Unit = {
+    maybeType match {
+      case Some(Type.AnonymousName()) =>
+      case Some(tpe) =>
+        typeRenderer.render(tpe)
+        write(" ")
+      case None =>
+        writeComment(UnknownType)
+        write(" ")
+    }
+  }
+
+  private def renderMethodParamsAndBody(defDef: Defn.Def, context: DefnDefRenderContext): Unit = {
+    val paramListRenderContext = TermParamListRenderContext(javaModifiers = context.paramJavaModifiers)
+    termParamListRenderer.render(defDef.paramss.flatten, paramListRenderContext)
+    blockCoercingTermRenderer.render(defDef.body, BlockRenderContext(uncertainReturn = defDef.decltpe.isEmpty))
+  }
+}
