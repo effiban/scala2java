97950773d178c28431f0e43ab8c9f5db1c7482e3
effiban <effi.ban@skai.io>
2023-07-28 20:20:48 +0300

    In `DefnRenderContextFactory` support `DefnVarTraversalResult` and `DefnDefTraversalResult`

--- scala2java-core/src/main/scala/io/github/effiban/scala2java/core/renderers/contextfactories/DefnRenderContextFactory.scala
+++ scala2java-core/src/main/scala/io/github/effiban/scala2java/core/renderers/contextfactories/DefnRenderContextFactory.scala
@@ -1,8 +1,8 @@
 package io.github.effiban.scala2java.core.renderers.contextfactories
 
 import io.github.effiban.scala2java.core.entities.SealedHierarchies
-import io.github.effiban.scala2java.core.renderers.contexts.{DefnRenderContext, UnsupportedDefnRenderContext}
-import io.github.effiban.scala2java.core.traversers.results.{DefnTraversalResult, TraitTraversalResult}
+import io.github.effiban.scala2java.core.renderers.contexts.{DefRenderContext, DefnRenderContext, UnsupportedDefnRenderContext, VarRenderContext}
+import io.github.effiban.scala2java.core.traversers.results.{DefnDefTraversalResult, DefnTraversalResult, DefnVarTraversalResult, TraitTraversalResult}
 
 trait DefnRenderContextFactory {
   def apply(DefnTraversalResult: DefnTraversalResult, sealedHierarchies: SealedHierarchies = SealedHierarchies()): DefnRenderContext
@@ -12,6 +12,8 @@ private[contextfactories] class DefnRenderContextFactoryImpl(traitRenderContextF
 
   override def apply(defnTraversalResult: DefnTraversalResult,
                      sealedHierarchies: SealedHierarchies = SealedHierarchies()): DefnRenderContext = defnTraversalResult match {
+    case defnVarTraversalResult: DefnVarTraversalResult => VarRenderContext(defnVarTraversalResult.javaModifiers)
+    case defnDefTraversalResult: DefnDefTraversalResult => DefRenderContext(defnDefTraversalResult.javaModifiers)
     case traitTraversalResult: TraitTraversalResult =>
       val permittedSubTypeNames = sealedHierarchies.getSubTypeNames(traitTraversalResult.name)
       traitRenderContextFactory(traitTraversalResult, permittedSubTypeNames)
