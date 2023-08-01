4b3874525fd4d833775adfcfc5d085cab57ebe0e
Effi Ban <effi.ban@kenshoo.com>
2023-08-01 23:09:53 +0300

    Revert "Add `ModListEnricher`"

--- scala2java-core/src/main/scala/io/github/effiban/scala2java/core/enrichers/entities/EnrichedModList.scala
+++
@@ -1,8 +0,0 @@
-package io.github.effiban.scala2java.core.enrichers.entities
-
-import io.github.effiban.scala2java.core.entities.JavaModifier
-
-import scala.meta.Mod
-
-case class EnrichedModList(scalaMods: List[Mod] = Nil, javaModifiers: List[JavaModifier] = Nil)
-  extends EnrichedWithJavaModifiers
--- scala2java-core/src/main/scala/io/github/effiban/scala2java/core/enrichers/entities/EnrichedWithJavaModifiers.scala
+++
@@ -1,7 +0,0 @@
-package io.github.effiban.scala2java.core.enrichers.entities
-
-import io.github.effiban.scala2java.core.entities.JavaModifier
-
-trait EnrichedWithJavaModifiers {
-  val javaModifiers: List[JavaModifier]
-}
