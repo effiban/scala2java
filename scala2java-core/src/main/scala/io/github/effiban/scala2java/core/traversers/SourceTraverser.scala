3294f172f11d9100461d8f50a31bcccecb2be31a
effiban <effi.ban@skai.io>
2023-08-05 19:35:53 +0300

    Remove enrichment logic from `SourceTraverser`

--- scala2java-core/src/main/scala/io/github/effiban/scala2java/core/traversers/SourceTraverser.scala
+++ scala2java-core/src/main/scala/io/github/effiban/scala2java/core/traversers/SourceTraverser.scala
@@ -1,19 +1,19 @@
 package io.github.effiban.scala2java.core.traversers
 
-import io.github.effiban.scala2java.core.traversers.results.{PopulatedStatTraversalResult, SourceTraversalResult}
+import io.github.effiban.scala2java.core.traversers.results.PopulatedStatTraversalResult
 
 import scala.meta.Source
 
 trait SourceTraverser {
-  def traverse(source: Source): SourceTraversalResult
+  def traverse(source: Source): Source
 }
 
 private[traversers] class SourceTraverserImpl(defaultStatTraverser: => DefaultStatTraverser) extends SourceTraverser {
 
   // source file
-  def traverse(source: Source): SourceTraversalResult = {
+  def traverse(source: Source): Source = {
     val statResults = source.stats.map(stat => defaultStatTraverser.traverse(stat))
       .collect { case result: PopulatedStatTraversalResult => result }
-    SourceTraversalResult(statResults)
+    Source(statResults.map(_.tree))
   }
 }
