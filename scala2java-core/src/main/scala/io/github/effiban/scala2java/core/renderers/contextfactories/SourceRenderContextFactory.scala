521894c37703d628b267446b83dc18e27889da0b
effiban <effi.ban@skai.io>
2023-08-05 18:07:44 +0300

    Migrate `SourceRenderContextFactory` to work with enriched objects

--- scala2java-core/src/main/scala/io/github/effiban/scala2java/core/renderers/contextfactories/SourceRenderContextFactory.scala
+++ scala2java-core/src/main/scala/io/github/effiban/scala2java/core/renderers/contextfactories/SourceRenderContextFactory.scala
@@ -1,10 +1,16 @@
 package io.github.effiban.scala2java.core.renderers.contextfactories
 
+import io.github.effiban.scala2java.core.enrichers.entities.EnrichedSource
+import io.github.effiban.scala2java.core.entities.SealedHierarchies
 import io.github.effiban.scala2java.core.renderers.contexts.SourceRenderContext
 import io.github.effiban.scala2java.core.traversers.results.SourceTraversalResult
 
 trait SourceRenderContextFactory {
+
+  @deprecated
   def apply(traversalResult: SourceTraversalResult): SourceRenderContext
+
+  def apply(enrichedSource: EnrichedSource): SourceRenderContext
 }
 
 private[contextfactories] class SourceRenderContextFactoryImpl(defaultStatRenderContextFactory: => DefaultStatRenderContextFactory)
@@ -14,6 +20,14 @@ private[contextfactories] class SourceRenderContextFactoryImpl(defaultStatRender
     val statRenderContextMap = traversalResult.statResults
       .map(statResult => (statResult.tree, statResult))
       .map { case (stat, statResult) => (stat, defaultStatRenderContextFactory(statResult)) }
+      .toMap
+    SourceRenderContext(statRenderContextMap)
+  }
+
+  def apply(enrichedSource: EnrichedSource): SourceRenderContext = {
+    val statRenderContextMap = enrichedSource.enrichedStats
+      .map(statResult => (statResult.stat, statResult))
+      .map { case (stat, statResult) => (stat, defaultStatRenderContextFactory(statResult, SealedHierarchies())) }
       .toMap
     SourceRenderContext(statRenderContextMap)
   }
