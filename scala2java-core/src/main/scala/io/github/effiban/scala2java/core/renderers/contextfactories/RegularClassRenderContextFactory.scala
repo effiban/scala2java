4e4d3c095bd698fa2188db23cd3eb9d6a34bdeea
effiban <effi.ban@skai.io>
2023-08-05 17:35:15 +0300

    Migrate `RegularClassRenderContextFactory` to work with enriched objects

--- scala2java-core/src/main/scala/io/github/effiban/scala2java/core/renderers/contextfactories/RegularClassRenderContextFactory.scala
+++ scala2java-core/src/main/scala/io/github/effiban/scala2java/core/renderers/contextfactories/RegularClassRenderContextFactory.scala
@@ -1,5 +1,6 @@
 package io.github.effiban.scala2java.core.renderers.contextfactories
 
+import io.github.effiban.scala2java.core.enrichers.entities.EnrichedRegularClass
 import io.github.effiban.scala2java.core.renderers.contexts.RegularClassRenderContext
 import io.github.effiban.scala2java.core.traversers.results.RegularClassTraversalResult
 
@@ -7,7 +8,10 @@ import scala.meta.Name
 
 trait RegularClassRenderContextFactory {
 
+  @deprecated
   def apply(regularClassTraversalResult: RegularClassTraversalResult, permittedSubTypeNames: List[Name] = Nil): RegularClassRenderContext
+
+  def apply(enrichedRegularClass: EnrichedRegularClass, permittedSubTypeNames: List[Name]): RegularClassRenderContext
 }
 
 private[contextfactories] class RegularClassRenderContextFactoryImpl(templateBodyRenderContextFactory: => TemplateBodyRenderContextFactory)
@@ -20,6 +24,17 @@ private[contextfactories] class RegularClassRenderContextFactoryImpl(templateBod
       javaModifiers = regularClassTraversalResult.javaModifiers,
       javaTypeKeyword = regularClassTraversalResult.javaTypeKeyword,
       maybeInheritanceKeyword = regularClassTraversalResult.maybeInheritanceKeyword,
+      permittedSubTypeNames = permittedSubTypeNames,
+      bodyContext = templateBodyContext
+    )
+  }
+
+  def apply(enrichedRegularClass: EnrichedRegularClass, permittedSubTypeNames: List[Name]): RegularClassRenderContext = {
+    val templateBodyContext = templateBodyRenderContextFactory(enrichedRegularClass.enrichedTemplate)
+    RegularClassRenderContext(
+      javaModifiers = enrichedRegularClass.javaModifiers,
+      javaTypeKeyword = enrichedRegularClass.javaTypeKeyword,
+      maybeInheritanceKeyword = enrichedRegularClass.maybeInheritanceKeyword,
       permittedSubTypeNames = permittedSubTypeNames,
       bodyContext = templateBodyContext
     )
