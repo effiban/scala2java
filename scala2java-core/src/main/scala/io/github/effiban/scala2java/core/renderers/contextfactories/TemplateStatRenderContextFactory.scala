0fc6c01efe49031b02bc1c6d4fefe1378816f9f0
effiban <effi.ban@skai.io>
2023-08-05 12:57:05 +0300

    Migrate `TemplateStatRenderContextFactory` to work with enriched objects

--- scala2java-core/src/main/scala/io/github/effiban/scala2java/core/renderers/contextfactories/TemplateStatRenderContextFactory.scala
+++ scala2java-core/src/main/scala/io/github/effiban/scala2java/core/renderers/contextfactories/TemplateStatRenderContextFactory.scala
@@ -1,10 +1,16 @@
 package io.github.effiban.scala2java.core.renderers.contextfactories
 
+import io.github.effiban.scala2java.core.enrichers.entities.{EnrichedCtorSecondary, EnrichedEnumConstantList, EnrichedStat}
+import io.github.effiban.scala2java.core.entities.SealedHierarchies
 import io.github.effiban.scala2java.core.renderers.contexts.{EnumConstantListRenderContext, TemplateStatRenderContext}
 import io.github.effiban.scala2java.core.traversers.results.{CtorSecondaryTraversalResult, EnumConstantListTraversalResult, PopulatedStatTraversalResult}
 
 trait TemplateStatRenderContextFactory {
+
+  @deprecated
   def apply(statTraversalResult: PopulatedStatTraversalResult): TemplateStatRenderContext
+
+  def apply(enrichedStat: EnrichedStat): TemplateStatRenderContext
 }
 
 private[contextfactories] class TemplateStatRenderContextFactoryImpl(ctorSecondaryRenderContextFactory: CtorSecondaryRenderContextFactory,
@@ -17,8 +23,24 @@ private[contextfactories] class TemplateStatRenderContextFactoryImpl(ctorSeconda
     case aStatTraversalResult => createWithDefaultFactory(aStatTraversalResult)
   }
 
+  override def apply(enrichedStat: EnrichedStat): TemplateStatRenderContext = enrichedStat match {
+    case enrichedCtorSecondary: EnrichedCtorSecondary => ctorSecondaryRenderContextFactory(enrichedCtorSecondary)
+    case _: EnrichedEnumConstantList => EnumConstantListRenderContext
+    case anEnrichedStat => createWithDefaultFactory(anEnrichedStat)
+  }
+
+  @deprecated
   private def createWithDefaultFactory(aStatTraversalResult: PopulatedStatTraversalResult) = {
     defaultStatRenderContextFactory(aStatTraversalResult) match {
+      case renderContext: TemplateStatRenderContext => renderContext
+      // TODO this shouldn't happen, but the render context hierarchy should be fixed to avoid this case altogether
+      case renderContext => throw new IllegalStateException(
+        s"The render context must extend TemplateStatRenderContext at this point but it is: $renderContext")
+    }
+  }
+
+  private def createWithDefaultFactory(anEnrichedStat: EnrichedStat) = {
+    defaultStatRenderContextFactory(anEnrichedStat, SealedHierarchies()) match {
       case renderContext: TemplateStatRenderContext => renderContext
       // TODO this shouldn't happen, but the render context hierarchy should be fixed to avoid this case altogether
       case renderContext => throw new IllegalStateException(
--- scala2java-core/src/test/scala/io/github/effiban/scala2java/core/renderers/contextfactories/TemplateStatRenderContextFactoryImplTest.scala
+++ scala2java-core/src/test/scala/io/github/effiban/scala2java/core/renderers/contextfactories/TemplateStatRenderContextFactoryImplTest.scala
@@ -1,14 +1,23 @@
 package io.github.effiban.scala2java.core.renderers.contextfactories
 
+import io.github.effiban.scala2java.core.enrichers.entities.{EnrichedCtorSecondary, EnrichedDefnVar, EnrichedEnumConstantList}
+import io.github.effiban.scala2java.core.entities.SealedHierarchies
 import io.github.effiban.scala2java.core.renderers.contexts.{CtorSecondaryRenderContext, EnumConstantListRenderContext, VarRenderContext}
 import io.github.effiban.scala2java.core.testsuites.UnitTestSuite
 import io.github.effiban.scala2java.core.traversers.results.{CtorSecondaryTraversalResult, DefnVarTraversalResult, EnumConstantListTraversalResult}
 
 class TemplateStatRenderContextFactoryImplTest extends UnitTestSuite {
 
+  @deprecated
   private val ctorSecondaryTraversalResult = mock[CtorSecondaryTraversalResult]
+  @deprecated
   private val enumConstantListTraversalResult = mock[EnumConstantListTraversalResult]
+  @deprecated
   private val defnVarTraversalResult = mock[DefnVarTraversalResult]
+
+  private val enrichedCtorSecondary = mock[EnrichedCtorSecondary]
+  private val enrichedEnumConstantList = mock[EnrichedEnumConstantList]
+  private val enrichedDefnVar = mock[EnrichedDefnVar]
 
   private val ctorSecondaryRenderContext = mock[CtorSecondaryRenderContext]
   private val varRenderContext = mock[VarRenderContext]
@@ -26,12 +35,26 @@ class TemplateStatRenderContextFactoryImplTest extends UnitTestSuite {
     templateStatRenderContextFactory(ctorSecondaryTraversalResult) shouldBe ctorSecondaryRenderContext
   }
 
+  test("apply() for an EnrichedCtorSecondary") {
+    when(ctorSecondaryRenderContextFactory(enrichedCtorSecondary)).thenReturn(ctorSecondaryRenderContext)
+    templateStatRenderContextFactory(enrichedCtorSecondary) shouldBe ctorSecondaryRenderContext
+  }
+
   test("apply() for an EnumConstantListTraversalResult") {
     templateStatRenderContextFactory(enumConstantListTraversalResult) shouldBe EnumConstantListRenderContext
+  }
+
+  test("apply() for an EnrichedEnumConstantList") {
+    templateStatRenderContextFactory(enrichedEnumConstantList) shouldBe EnumConstantListRenderContext
   }
 
   test("apply() for a DefnVarTraversalResult") {
     when(defaultStatRenderContextFactory(defnVarTraversalResult)).thenReturn(varRenderContext)
     templateStatRenderContextFactory(defnVarTraversalResult) shouldBe varRenderContext
+  }
+
+  test("apply() for a EnrichedDefnVar") {
+    when(defaultStatRenderContextFactory(enrichedDefnVar, SealedHierarchies())).thenReturn(varRenderContext)
+    templateStatRenderContextFactory(enrichedDefnVar) shouldBe varRenderContext
   }
 }
