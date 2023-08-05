0a6013861ad6068608e57f5ecb55e030eab007e7
effiban <effi.ban@skai.io>
2023-08-05 12:29:28 +0300

    Migrate `CtorSecondaryRenderContextFactory` to work with enriched objects

--- scala2java-core/src/test/scala/io/github/effiban/scala2java/core/renderers/contextfactories/CtorSecondaryRenderContextFactoryTest.scala
+++ scala2java-core/src/test/scala/io/github/effiban/scala2java/core/renderers/contextfactories/CtorSecondaryRenderContextFactoryTest.scala
@@ -1,5 +1,6 @@
 package io.github.effiban.scala2java.core.renderers.contextfactories
 
+import io.github.effiban.scala2java.core.enrichers.entities.EnrichedCtorSecondary
 import io.github.effiban.scala2java.core.entities.JavaModifier
 import io.github.effiban.scala2java.core.renderers.contexts.CtorSecondaryRenderContext
 import io.github.effiban.scala2java.core.renderers.matchers.CtorSecondaryRenderContextScalatestMatcher.equalCtorSecondaryRenderContext
@@ -13,14 +14,26 @@ class CtorSecondaryRenderContextFactoryTest extends UnitTestSuite {
   private val TheClassName = t"MyClass"
   private val TheJavaModifiers = List(JavaModifier.Public, JavaModifier.Final)
 
+  @deprecated
   private val ctorSecondaryTraversalResult = mock[CtorSecondaryTraversalResult]
 
-  test("apply()") {
+  private val enrichedCtorSecondary = mock[EnrichedCtorSecondary]
+
+  test("apply() to CtorSecondaryTraversalResult") {
     val expectedRenderContext = CtorSecondaryRenderContext(TheClassName, TheJavaModifiers)
 
     when(ctorSecondaryTraversalResult.className).thenReturn(TheClassName)
     when(ctorSecondaryTraversalResult.javaModifiers).thenReturn(TheJavaModifiers)
 
     CtorSecondaryRenderContextFactory(ctorSecondaryTraversalResult) should equalCtorSecondaryRenderContext(expectedRenderContext)
+  }
+
+  test("apply() to EnrichedCtorSecondary") {
+    val expectedRenderContext = CtorSecondaryRenderContext(TheClassName, TheJavaModifiers)
+
+    when(enrichedCtorSecondary.className).thenReturn(TheClassName)
+    when(enrichedCtorSecondary.javaModifiers).thenReturn(TheJavaModifiers)
+
+    CtorSecondaryRenderContextFactory(enrichedCtorSecondary) should equalCtorSecondaryRenderContext(expectedRenderContext)
   }
 }
