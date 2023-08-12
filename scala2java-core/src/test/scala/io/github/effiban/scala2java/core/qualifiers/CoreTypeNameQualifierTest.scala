75da00a1c6cce0c5ac941d9af2be6e5485b2f8d8
effiban <effi.ban@skai.io>
2023-08-12 23:29:49 +0300

    Add `CoreTypeNameQualifier`

---
+++ scala2java-core/src/test/scala/io/github/effiban/scala2java/core/qualifiers/CoreTypeNameQualifierTest.scala
@@ -0,0 +1,41 @@
+package io.github.effiban.scala2java.core.qualifiers
+
+import io.github.effiban.scala2java.core.qualifiers.CoreTypeNameQualifier.qualify
+import io.github.effiban.scala2java.core.testsuites.UnitTestSuite
+
+import scala.meta.{Type, XtensionQuasiquoteType}
+
+class CoreTypeNameQualifierTest extends UnitTestSuite {
+
+  private final val PositiveScenarios = Table(
+    ("Type", "QualifiedType"),
+    (t"Any", t"scala.Any"),
+    (t"AnyRef", t"scala.AnyRef"),
+    (t"Boolean", t"scala.Boolean"),
+    (t"Byte", t"scala.Byte"),
+    (t"Char", t"scala.Char"),
+    (t"Short", t"scala.Short"),
+    (t"Int", t"scala.Int"),
+    (t"Long", t"scala.Long"),
+    (t"Float", t"scala.Float"),
+    (t"Double", t"scala.Double"),
+    (t"Unit", t"scala.Unit"),
+    (t"Seq", t"scala.collection.immutable.Seq"),
+    (t"Vector", t"scala.collection.immutable.Vector"),
+    (t"List", t"scala.collection.immutable.List"),
+    (t"Set", t"scala.collection.immutable.Set"),
+    (t"Map", t"scala.collection.immutable.Map"),
+    (t"Option", t"scala.Option"),
+    (t"Future", t"scala.concurrent.Future")
+  )
+
+  forAll(PositiveScenarios) { (tpe: Type.Name, expectedQualifiedType: Type.Select) =>
+    test(s"qualified type of $tpe should be $expectedQualifiedType") {
+      qualify(tpe).value.structure shouldBe expectedQualifiedType.structure
+    }
+  }
+
+  test("qualify() when unmapped should return None") {
+    qualify(t"Bla") shouldBe None
+  }
+}
