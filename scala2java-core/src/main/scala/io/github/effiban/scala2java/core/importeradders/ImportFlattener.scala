6e8e739a70171753acb4219756e27ed9d9c03e9f
effiban <effi.ban@skai.io>
2023-08-12 08:04:47 +0300

    Add `ImportFlattener`

---
+++ scala2java-core/src/main/scala/io/github/effiban/scala2java/core/importeradders/ImportFlattener.scala
@@ -0,0 +1,15 @@
+package io.github.effiban.scala2java.core.importeradders
+
+import scala.meta.{Import, Importer}
+
+trait ImportFlattener {
+  def flatten(imports: List[Import]): List[Importer]
+}
+
+object ImportFlattener extends ImportFlattener {
+
+  def flatten(imports: List[Import]): List[Importer] = {
+    imports.flatMap(`import` => `import`.importers)
+      .flatMap { importer => importer.importees.map(importee => importer.copy(importees = List(importee))) }
+  }
+}
---
+++ scala2java-core/src/test/scala/io/github/effiban/scala2java/core/importeradders/ImportFlattenerTest.scala
@@ -0,0 +1,61 @@
+package io.github.effiban.scala2java.core.importeradders
+
+import io.github.effiban.scala2java.core.importeradders.ImportFlattener.flatten
+import io.github.effiban.scala2java.core.testsuites.UnitTestSuite
+
+import scala.meta.{XtensionQuasiquoteImporter, XtensionQuasiquoteTerm}
+
+class ImportFlattenerTest extends UnitTestSuite {
+
+  test("flatten when empty should return empty") {
+    flatten(Nil) shouldBe Nil
+  }
+
+  test("flatten for: one import, one importer, one importee") {
+    flatten(List(q"import a.b")).structure shouldBe List(importer"a.b").structure
+  }
+
+  test("flatten for: one import, one importer, two importees") {
+    flatten(List(q"import a.{b, c}")).structure shouldBe List(importer"a.b", importer"a.c").structure
+  }
+
+  test("flatten for: one import, two importers, two importees each, all have names") {
+    val `import` = q"import a.{b, c}, d.{e, f}"
+    val expectedImporters = List(
+      importer"a.b",
+      importer"a.c",
+      importer"d.e",
+      importer"d.f"
+    )
+    flatten(List(`import`)).structure shouldBe expectedImporters.structure
+  }
+
+  test("flatten for: one import, two importers, with mixture of names and wildcards") {
+    val `import` = q"import a.{b, c}, d._"
+    val expectedImporters = List(
+      importer"a.b",
+      importer"a.c",
+      importer"d._"
+    )
+    flatten(List(`import`)).structure shouldBe expectedImporters.structure
+  }
+
+  test("flatten for two imports, two importers each, two importees each") {
+    val imports = List(
+      q"import a.{b, c}, d.{e, f}",
+      q"import g.{h, i}, j.{k, l}"
+    )
+
+    val expectedImporters = List(
+      importer"a.b",
+      importer"a.c",
+      importer"d.e",
+      importer"d.f",
+      importer"g.h",
+      importer"g.i",
+      importer"j.k",
+      importer"j.l",
+    )
+    flatten(imports).structure shouldBe expectedImporters.structure
+  }
+}
