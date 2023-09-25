659f7095c8e0c600e9dea67cf2d507c59f7048aa
Effi Ban <effi.ban@skai.io>
2023-09-25 15:33:31 +0300

    Fix recursion of `TreeImporterUsed`

--- scala2java-core/src/test/scala/io/github/effiban/scala2java/core/importmanipulation/TreeImporterUsedImplTest.scala
+++ scala2java-core/src/test/scala/io/github/effiban/scala2java/core/importmanipulation/TreeImporterUsedImplTest.scala
@@ -2,8 +2,9 @@ package io.github.effiban.scala2java.core.importmanipulation
 
 import io.github.effiban.scala2java.core.testsuites.UnitTestSuite
 import io.github.effiban.scala2java.test.utils.matchers.TreeMatcher.eqTree
+import org.mockito.Mockito.verifyNoInteractions
 
-import scala.meta.{XtensionQuasiquoteImporter, XtensionQuasiquoteTerm, XtensionQuasiquoteType}
+import scala.meta.{Type, XtensionQuasiquoteImporter, XtensionQuasiquoteTerm, XtensionQuasiquoteType, XtensionQuasiquoteTypeParam}
 
 class TreeImporterUsedImplTest extends UnitTestSuite {
 
@@ -12,26 +13,95 @@ class TreeImporterUsedImplTest extends UnitTestSuite {
   private val treeImporterUsed = new TreeImporterUsedImpl(typeNameImporterMatcher)
 
 
-  test("apply() for a Pkg when has a matching Type.Name should return true") {
-    val pkg =
-      q"""
-      package a.b {
-        import d.D
+  test("apply() for a 'Decl.Var' when has a matching Type.Name should return true") {
+    val importer = importer"x.X"
 
-        trait C {
-          val x: D
-        }
+    doReturn(Some(importer)).when(typeNameImporterMatcher).findMatch(eqTree(t"X"), eqTree(importer))
+
+    treeImporterUsed(q"var x: X", importer) shouldBe true
+  }
+
+  test("apply() for a 'Decl.Var' when has no matching Type.Name should return false") {
+    val importer = importer"y.Y"
+
+    doReturn(None).when(typeNameImporterMatcher).findMatch(eqTree(t"X"), eqTree(importer))
+
+    treeImporterUsed(q"var x: X", importer) shouldBe false
+  }
+
+  test("apply() for a Class when has a matching nested Type.Name should return true") {
+    val theClass =
+      q"""
+      class A {
+        var b: B
       }
       """
 
-    doReturn(Some(importer"d.D")).when(typeNameImporterMatcher).findMatch(eqTree(t"D"), eqTree(importer"d.D"))
+    val importer = importer"b.B"
 
-    treeImporterUsed(pkg, importer"d.D") shouldBe true
+    doReturn(Some(importer)).when(typeNameImporterMatcher).findMatch(eqTree(t"B"), eqTree(importer))
+
+    treeImporterUsed(theClass, importer) shouldBe true
   }
 
-  test("apply() for a Pkg when has Type.Names and none match should return false") {
+  test("apply() for a Class when has no matching nested trees should return false") {
+    val theClass =
+      q"""
+      class A {
+        var b: B
+      }
+      """
+
+    val importer = importer"c.C"
+
+    doReturn(None).when(typeNameImporterMatcher).findMatch(eqTree(t"B"), eqTree(importer))
+
+    treeImporterUsed(theClass, importer) shouldBe false
   }
 
-  test("apply() for a Pkg when has no Type.Names should return false") {
+  test("apply() for a Trait when has a matching nested Type.Name should return true") {
+    val theClass =
+      q"""
+      trait A {
+        var b: B
+      }
+      """
+
+    val importer = importer"b.B"
+
+    doReturn(Some(importer)).when(typeNameImporterMatcher).findMatch(eqTree(t"B"), eqTree(importer))
+
+    treeImporterUsed(theClass, importer) shouldBe true
+  }
+
+  test("apply() for a Trait when has no matching nested trees should return false") {
+    val theTrait =
+      q"""
+      trait A {
+        val b: B
+      }
+      """
+
+    val importer = importer"c.C"
+
+    doReturn(None).when(typeNameImporterMatcher).findMatch(eqTree(t"B"), eqTree(importer))
+
+    treeImporterUsed(theTrait, importer) shouldBe false
+  }
+
+  test("apply() for a Type.Var should not try to match it and return false") {
+    val importer = importer"a.b"
+
+    treeImporterUsed(Type.Var(t"b"), importer) shouldBe false
+
+    verifyNoInteractions(typeNameImporterMatcher)
+  }
+
+  test("apply() for the name of a Type.Param should not try to match it and return false") {
+    val importer = importer"t.T"
+
+    treeImporterUsed(tparam"T", importer) shouldBe false
+
+    verifyNoInteractions(typeNameImporterMatcher)
   }
 }
