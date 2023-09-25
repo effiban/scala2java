b78fb50d64bb927ccf33554f2212bd07247613c2
Effi Ban <effi.ban@skai.io>
2023-09-25 17:01:11 +0300

    Qualify core standalone `Term.Name`s at start of flow

--- scala2java-core/src/integrationTest/resources/testfiles/DefnVals/WithoutDeclaredType/InClass/Inferrable/TermName/None/Sample.java
+++ scala2java-core/src/integrationTest/resources/testfiles/DefnVals/WithoutDeclaredType/InClass/Inferrable/TermName/None/Sample.java
@@ -1,7 +1,8 @@
 package dummy;
 
 import java.util.Optional;
+import java.util.Optional.empty;
 
 public class Sample {
-    public final Optional x = Optional.absent();
+    public final Optional x = empty();
 }
