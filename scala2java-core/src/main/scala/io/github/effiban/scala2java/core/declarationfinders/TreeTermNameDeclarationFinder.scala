109394afcbb050af5c6bc3eb8639505887b63fea
effiban <effi.ban@skai.io>
2024-06-12 17:28:30 +0300

    Add and invoke `DeclVarTermNameDeclarationFinder`

--- scala2java-core/src/main/scala/io/github/effiban/scala2java/core/declarationfinders/TreeTermNameDeclarationFinder.scala
+++ scala2java-core/src/main/scala/io/github/effiban/scala2java/core/declarationfinders/TreeTermNameDeclarationFinder.scala
@@ -1,20 +1,23 @@
 package io.github.effiban.scala2java.core.declarationfinders
 
-import scala.meta.{Term, Tree}
+import scala.meta.{Decl, Term, Tree}
 
 trait TreeTermNameDeclarationFinder {
   def find(tree: Tree, termName: Term.Name): Option[Tree]
 }
 
 private[declarationfinders] class TreeTermNameDeclarationFinderImpl(
-  termParamTermNameDeclarationFinder: TermParamTermNameDeclarationFinder) extends TreeTermNameDeclarationFinder {
+  termParamTermNameDeclarationFinder: TermParamTermNameDeclarationFinder,
+  declVarTermNameDeclarationFinder: DeclVarTermNameDeclarationFinder) extends TreeTermNameDeclarationFinder {
 
   override def find(tree: Tree, termName: Term.Name): Option[Tree] = tree match {
     case termParam: Term.Param => termParamTermNameDeclarationFinder.find(termParam, termName)
+    case declVar: Decl.Var => declVarTermNameDeclarationFinder.find(declVar, termName)
     case _ => None
   }
 }
 
 object TreeTermNameDeclarationFinder extends TreeTermNameDeclarationFinderImpl(
-  TermParamTermNameDeclarationFinder
+  TermParamTermNameDeclarationFinder,
+  DeclVarTermNameDeclarationFinder
 )
--- scala2java-core/src/test/scala/io/github/effiban/scala2java/core/declarationfinders/TreeTermNameDeclarationFinderImplTest.scala
+++ scala2java-core/src/test/scala/io/github/effiban/scala2java/core/declarationfinders/TreeTermNameDeclarationFinderImplTest.scala
@@ -3,16 +3,20 @@ package io.github.effiban.scala2java.core.declarationfinders
 import io.github.effiban.scala2java.core.testsuites.UnitTestSuite
 import io.github.effiban.scala2java.test.utils.matchers.TreeMatcher.eqTree
 
-import scala.meta.{XtensionQuasiquoteTerm, XtensionQuasiquoteTermParam}
+import scala.meta.{XtensionQuasiquoteCaseOrPattern, XtensionQuasiquoteTerm, XtensionQuasiquoteTermParam}
 
 class TreeTermNameDeclarationFinderImplTest extends UnitTestSuite {
 
   private val termParamTermNameDeclarationFinder = mock[TermParamTermNameDeclarationFinder]
+  private val declVarTermNameDeclarationFinder = mock[DeclVarTermNameDeclarationFinder]
 
-  private val treeTermNameDeclarationFinder = new TreeTermNameDeclarationFinderImpl(termParamTermNameDeclarationFinder)
+  private val treeTermNameDeclarationFinder = new TreeTermNameDeclarationFinderImpl(
+    termParamTermNameDeclarationFinder,
+    declVarTermNameDeclarationFinder
+  )
 
   test("find() for Term.Param when found") {
-    val termParam = param"x: Int"
+    val termParam = param"x: scala.Int"
     val termName = q"x"
 
     when(termParamTermNameDeclarationFinder.find(eqTree(termParam), eqTree(termName))).thenReturn(Some(termParam))
@@ -21,11 +25,30 @@ class TreeTermNameDeclarationFinderImplTest extends UnitTestSuite {
   }
 
   test("find() for Term.Param when not found") {
-    val termParam = param"x: Int"
+    val termParam = param"x: scala.Int"
     val termName = q"y"
 
     when(termParamTermNameDeclarationFinder.find(eqTree(termParam), eqTree(termName))).thenReturn(None)
 
     treeTermNameDeclarationFinder.find(termParam, termName) shouldBe None
+  }
+
+  test("find() for Decl.Var when found") {
+    val declVar = q"var x: scala.Int"
+    val termName = q"x"
+    val patVar = p"x"
+
+    when(declVarTermNameDeclarationFinder.find(eqTree(declVar), eqTree(termName))).thenReturn(Some(patVar))
+
+    treeTermNameDeclarationFinder.find(declVar, termName).value.structure shouldBe patVar.structure
+  }
+
+  test("find() for Decl.Var when not found") {
+    val declVar = q"var x: scala.Int"
+    val termName = q"y"
+
+    when(declVarTermNameDeclarationFinder.find(eqTree(declVar), eqTree(termName))).thenReturn(None)
+
+    treeTermNameDeclarationFinder.find(declVar, termName) shouldBe None
   }
 }
