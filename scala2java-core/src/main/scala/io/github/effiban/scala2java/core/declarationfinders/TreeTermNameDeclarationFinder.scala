3510f716e117c93592e7e47001388c41aea2a29e
effiban <effi.ban@skai.io>
2024-06-14 13:56:36 +0300

    Support declaration finders for trees with `Term.Param`-s

--- scala2java-core/src/main/scala/io/github/effiban/scala2java/core/declarationfinders/TreeTermNameDeclarationFinder.scala
+++ scala2java-core/src/main/scala/io/github/effiban/scala2java/core/declarationfinders/TreeTermNameDeclarationFinder.scala
@@ -1,6 +1,6 @@
 package io.github.effiban.scala2java.core.declarationfinders
 
-import scala.meta.{Decl, Defn, Term, Tree}
+import scala.meta.{Decl, Defn, Template, Term, Tree}
 
 trait TreeTermNameDeclarationFinder {
   def find(tree: Tree, termName: Term.Name): Option[Tree]
@@ -15,7 +15,15 @@ private[declarationfinders] class TreeTermNameDeclarationFinderImpl(
     case termParam: Term.Param => termParamTermNameDeclarationFinder.find(termParam, termName)
     case declVar: Decl.Var => declVarTermNameDeclarationFinder.find(declVar, termName)
     case defnVar: Defn.Var => defnVarTermNameDeclarationFinder.find(defnVar, termName)
+    case declDef: Decl.Def => find(declDef.paramss.flatten, termName)
+    case defnDef: Defn.Def => find(defnDef.paramss.flatten, termName)
+    case function: Term.Function => find(function.params, termName)
+    case aClass: Defn.Class => find(aClass.ctor.paramss.flatten, termName)
     case _ => None
+  }
+
+  private def find(params: List[Term.Param], termName: Term.Name) = {
+    params.flatMap(param => termParamTermNameDeclarationFinder.find(param, termName)).headOption
   }
 }
 
