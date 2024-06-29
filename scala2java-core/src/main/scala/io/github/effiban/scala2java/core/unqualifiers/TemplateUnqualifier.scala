c556709977b2c041f53ecaa17051743dd3fbfa27
effiban <effi.ban@skai.io>
2024-06-29 17:59:06 +0300

    Add `TemplateUnqualifier`

---
+++ scala2java-core/src/main/scala/io/github/effiban/scala2java/core/unqualifiers/TemplateUnqualifier.scala
@@ -0,0 +1,29 @@
+package io.github.effiban.scala2java.core.unqualifiers
+
+import io.github.effiban.scala2java.core.qualifiers.QualificationContext
+import io.github.effiban.scala2java.core.reflection.ScalaReflectionUtils.{asScalaMetaTypeRef, classSymbolOf}
+
+import scala.meta.{Init, Self, Stat, Template}
+
+trait TemplateUnqualifier {
+  def unqualify(template: Template, context: QualificationContext = QualificationContext()): Template
+}
+
+private[unqualifiers] class TemplateUnqualifierImpl(treeUnqualifier: => TreeUnqualifier) extends TemplateUnqualifier {
+
+  override def unqualify(template: Template, context: QualificationContext = QualificationContext()): Template = {
+
+    // The template children must be qualified before the template so that the inits (parents) will still be qualified when
+    // the children need to examine them (for example 'super.x()' needs to examine the parents to do the unqualification properly,
+    // since they are Java parents that are not (necessarily) the compiled Scala parents of the class, and cannot be obtained by reflection).
+
+    // TODO - handle "early" definitions
+    val unqualifiedInits = template.inits.map(init => treeUnqualifier.unqualify(init, context).asInstanceOf[Init])
+    val unqualifiedSelf = treeUnqualifier.unqualify(template.self, context).asInstanceOf[Self]
+
+    val parentUnqualifiedTemplate = template.copy(inits = unqualifiedInits, self = unqualifiedSelf)
+
+    val unqualifiedStats = template.stats.map(stat => treeUnqualifier.unqualify(stat, context).asInstanceOf[Stat])
+    parentUnqualifiedTemplate.copy(stats = unqualifiedStats)
+  }
+}
