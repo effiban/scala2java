63856f0d7d1c7f02a6f6b5ddd4d3795da1934c6d
effiban <effi.ban@skai.io>
2025-07-05 11:51:05 +0300

    FIx `TypeSingletonRenderer`

--- scala2java-core/src/main/scala/io/github/effiban/scala2java/core/renderers/TypeSingletonRenderer.scala
+++ scala2java-core/src/main/scala/io/github/effiban/scala2java/core/renderers/TypeSingletonRenderer.scala
@@ -1,20 +1,12 @@
 package io.github.effiban.scala2java.core.renderers
 
-import io.github.effiban.scala2java.core.writers.JavaWriter
-
-import scala.meta.{Term, Type}
+import scala.meta.Type
 
 trait TypeSingletonRenderer extends JavaTreeRenderer[Type.Singleton]
 
-private[renderers] class TypeSingletonRendererImpl(thisRenderer: ThisRenderer)
-                                                  (implicit javaWriter: JavaWriter) extends TypeSingletonRenderer {
-
-  import javaWriter._
+private[renderers] class TypeSingletonRendererImpl(termRefRenderer: TermRefRenderer) extends TypeSingletonRenderer {
 
   override def render(singletonType: Type.Singleton): Unit = {
-    singletonType.ref match {
-      case `this`: Term.This => thisRenderer.render(`this`)
-      case aRef => writeComment(s"$aRef.type")
-    }
+    termRefRenderer.render(singletonType.ref)
   }
 }
