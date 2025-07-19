86af6348be78a0c57ab18d387fa6ae0b4c231ba7
effiban <effi.ban@skai.io>
2025-07-19 17:21:57 +0300

    Support type inference of a singleton member, when the singleton is provided by type

---
+++ scala2java-core/src/test/scala/io/github/effiban/scala2java/core/reflection/TestAliases.scala
@@ -0,0 +1,5 @@
+package io.github.effiban.scala2java.core.reflection
+
+private[reflection] object TestAliases {
+  val TestObject: io.github.effiban.scala2java.core.reflection.TestObject.type = io.github.effiban.scala2java.core.reflection.TestObject
+}
