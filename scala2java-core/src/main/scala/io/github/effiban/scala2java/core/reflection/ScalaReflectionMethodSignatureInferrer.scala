3667a9eb98e96fec302b3cd004345b0e9d917040
effiban <effi.ban@skai.io>
2025-08-01 23:08:22 +0300

    Extract `ScalaReflectionInternalMethodSignatureInferrer`

--- scala2java-core/src/main/scala/io/github/effiban/scala2java/core/reflection/ScalaReflectionMethodSignatureInferrer.scala
+++ scala2java-core/src/main/scala/io/github/effiban/scala2java/core/reflection/ScalaReflectionMethodSignatureInferrer.scala
@@ -18,49 +18,15 @@ object ScalaReflectionMethodSignatureInferrer extends ScalaReflectionMethodSigna
 
   def inferPartialMethodSignature(qualType: Type.Ref, name: Term.Name, argTypes: List[Type]): PartialDeclDef = {
     toClassSymbol(qualType) match {
-      case Some(qualCls) => inferPartialMethodSignature(qualCls, name, argTypes)
+      case Some(qualCls) => ScalaReflectionInternalMethodSignatureInferrer.inferPartialMethodSignature(qualCls, name, argTypes)
       case _ => PartialDeclDef()
     }
   }
 
   def inferPartialMethodSignature(qual: Term.Ref, name: Term.Name, argTypes: List[Type]): PartialDeclDef = {
     findModuleSymbolOf(qual.toString()) match {
-      case Some(qualSymbol) => inferPartialMethodSignature(qualSymbol, name, argTypes)
+      case Some(qualSymbol) => ScalaReflectionInternalMethodSignatureInferrer.inferPartialMethodSignature(qualSymbol, name, argTypes)
       case _ => PartialDeclDef()
     }
-  }
-
-  private def inferPartialMethodSignature(qualSym: Symbol, name: Term.Name, smArgTypes: List[Type]): PartialDeclDef = {
-    qualSym.info.member(TermName(name.value)).alternatives
-      .collect { case method: MethodSymbol => method }
-      .map(method => resolvePartialMethodSignatureIfMatches(method, smArgTypes))
-      .collectFirst { case partialDeclDef: PartialDeclDef if partialDeclDef.nonEmpty => partialDeclDef }
-      .getOrElse(PartialDeclDef())
-  }
-
-  private def resolvePartialMethodSignatureIfMatches(method: MethodSymbol, smArgTypes: List[Type]): PartialDeclDef = {
-    if (methodMatchesScalaMetaArgs(method, smArgTypes)) resolveScalaMetaPartialMethodSignature(method) else PartialDeclDef()
-  }
-
-  private def methodMatchesScalaMetaArgs(method: MethodSymbol, smArgTypes: List[Type]): Boolean = {
-    method.paramLists match {
-      case List(params: List[Symbol]) => paramTypesMatchScalaMetaArgTypes(params, smArgTypes)
-      case Nil if smArgTypes.isEmpty => true
-      case _ => false
-    }
-  }
-
-  private def paramTypesMatchScalaMetaArgTypes(params: List[Symbol], smArgTypes: List[Type]): Boolean = {
-    params.size == smArgTypes.size &&
-    params.indices.forall(idx => paramMatchesScalaMetaArg(params(idx), smArgTypes(idx)))
-  }
-
-  private def paramMatchesScalaMetaArg(param: Symbol, smArgType: Type): Boolean = {
-    val paramTypeFullName = finalResultTypeFullnameOf(param)
-    toClassSymbol(smArgType).map(_.fullName).contains(paramTypeFullName)
-  }
-
-  private def resolveScalaMetaPartialMethodSignature(method: MethodSymbol): PartialDeclDef = {
-    toScalaMetaPartialDeclDef(method)
   }
 }
